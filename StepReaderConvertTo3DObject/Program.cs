749455fe6e3ac0771628b8c2be34eb1b21a69b92
sypark <sypark@fourthlogic.co.kr>
2024-07-19 19:31:01 +0900

    [StepReaderConvertTo3DObject] Optimize the method of displaying objects in the 3DView for better performance.

--- StepReaderConvertTo3DObject/Program.cs
+++ StepReaderConvertTo3DObject/Program.cs
@@ -51,60 +51,104 @@ namespace StepReaderConvertTo3DObject
                 view3D[0].SetTopologyType(ETopologyType3D.Wireframe);
                 view3D[1].SetTopologyType(ETopologyType3D.Wireframe);
 
-                //곡선의 접선에서 코드(Chord, 곡선의 두 점을 직선으로 연결한 선분)가 벗어날 수 있는 최대 거리를 나타냅니다. 
-                // 이 값이 클수록 분할된 삼각형의 수가 적어지며, 
-                // 반대로 값이 작을수록 더 많은 삼각형이 형성되어 곡선을 더 정밀하게 근사합니다. 
-                // 기본값 0을 입력하면 step 모델에서 적절한 chordal deviation 값을 자동으로 계산합니다.
-                // It represents the maximum distance that a chord (a straight line segment connecting two points on the curve) can deviate from the tangent of the curve. 
-                // A larger value results in fewer triangles being formed, 
-                // while a smaller value results in more triangles, providing a more precise approximation of the curve. 
-                // The default value of 0 automatically calculates the appropriate chordal deviation value based on the imported step model.
-                double f64ChordalDeviation = 0;
+				// 우선 빈 CGUIView3DObject 객체를 뷰에 추가한 후 해당 객체의 인덱스를 i32ReturnIndex 에 얻어 오기
+				// First, add an empty CGUIView3DObject object to the view, then retrieve the index of that object into i32ReturnIndex.
+				int i32ReturnIndex = -1;
+				if((res = view3D[0].PushObject(new CGUIView3DObject(), out i32ReturnIndex)).IsFail())
+				{
+					ErrorPrint(res, "Failed to display 3D object.\n");
+					break;
+				}
 
-                // 방법 1. CFL3DObject 에서 Step 파일 로드
-                // Method 1. Load the STEP file in CFL3DObject
-                CFL3DObject fl3DObj0 = new CFL3DObject();
+				// 뷰에 추가된 CGUIView3DObject 객체를 i32ReturnIndex 를 이용해서 얻어 오기
+				// Retrieve the CGUIView3DObject object added to the view using i32ReturnIndex.
+				CGUIView3DObject objView3D = view3D[0].GetView3DObject(i32ReturnIndex);
+				if(objView3D == null)
+				{
+					ErrorPrint(res, "Failed to display 3D object.\n");
+					break;
+				}
 
-                if ((res = fl3DObj0.LoadSTEP("../../ExampleImages/StepReaderConvertTo3DObject/Cylinder.step", f64ChordalDeviation)).IsFail())
-                {
-                    ErrorPrint(res, "Failed to load step file.\n");
-                    break;
-                }
+				// 뷰에 추가된 CGUIView3DObject 객체의 내부 CFL3DObject 포인터를 얻어 오기
+				// Retrieve the internal CFL3DObject pointer of the CGUIView3DObject object added to the view.
+				CFL3DObject fl3DObject = objView3D.GetData();
+				if(fl3DObject == null)
+				{
+					ErrorPrint(res, "Failed to display 3D object.\n");
+					break;
+				}
 
-                if ((res = view3D[0].PushObject(fl3DObj0)).IsFail())
-                {
-                    ErrorPrint(res, "Failed to display 3D object.\n");
-                    break;
-                }
+				//곡선의 접선에서 코드(Chord, 곡선의 두 점을 직선으로 연결한 선분)가 벗어날 수 있는 최대 거리를 나타냅니다. 
+				// 이 값이 클수록 분할된 삼각형의 수가 적어지며, 
+				// 반대로 값이 작을수록 더 많은 삼각형이 형성되어 곡선을 더 정밀하게 근사합니다. 
+				// 기본값 0을 입력하면 step 모델에서 적절한 chordal deviation 값을 자동으로 계산합니다.
+				// It represents the maximum distance that a chord (a straight line segment connecting two points on the curve) can deviate from the tangent of the curve. 
+				// A larger value results in fewer triangles being formed, 
+				// while a smaller value results in more triangles, providing a more precise approximation of the curve. 
+				// The default value of 0 automatically calculates the appropriate chordal deviation value based on the imported step model.
+				double f64ChordalDeviation = 0;
 
+				// 방법 1. CFL3DObject 에서 Step 파일 로드
+				// Method 1. Load the STEP file in CFL3DObject
+
+				// 뷰에 추가된 CGUIView3DObject 객체의 내부 CFL3DObject 에 STEP 파일 로드
+				// Load the STEP file into the internal CFL3DObject of the CGUIView3DObject object added to the view.
+
+				if((res = fl3DObject.LoadSTEP("../../ExampleImages/StepReaderConvertTo3DObject/Cylinder.step", f64ChordalDeviation)).IsFail())
+				{
+					ErrorPrint(res, "Failed to load step file.\n");
+					break;
+				}
+
+				// CFL3DObject 에 STEP 파일을 로드하였으므로 뷰의 CGUIView3DObject 객체를 업데이트
+				// Since the STEP file has been loaded into the CFL3DObject, update the CGUIView3DObject object in the view.
+				view3D[0].UpdateObject(i32ReturnIndex);
+				view3D[0].ZoomFit();
 
                 // 방법 2. CStepReader 에서 Step 파일 로드 후 GetResult3DObject() 로 CFL3DObject 에 할당
-                // Method 2. Load the STEP file using CStepReader and then assign it to CFL3DObject using GetResult3DObject().
-                CFL3DObject fl3DObj1;
+                // Method 2. Load the STEP file using CStepReader and then assign it to CFL3DObject using GetResult3DObject().                
                 CStepReader sr = new CStepReader();
                 f64ChordalDeviation = 0.00001;
 
-                if ((res = sr.Load("../../ExampleImages/StepReaderConvertTo3DObject/Cylinder.step")).IsFail())
+				i32ReturnIndex = -1;
+				if((res = view3D[1].PushObject(new CGUIView3DObject(), out i32ReturnIndex)).IsFail())
+				{
+					ErrorPrint(res, "Failed to display 3D object.\n");
+					break;
+				}
+
+				CGUIView3DObject objView3D2 = view3D[1].GetView3DObject(i32ReturnIndex);
+				if(objView3D2 == null)
+				{
+					ErrorPrint(res, "Failed to display 3D object.\n");
+					break;
+				}
+
+				CFL3DObject fl3DObject2 = objView3D2.GetData();
+				if(fl3DObject2 == null)
+				{
+					ErrorPrint(res, "Failed to display 3D object.\n");
+					break;
+				}
+
+				if ((res = sr.Load("../../ExampleImages/StepReaderConvertTo3DObject/Cylinder.step")).IsFail())
                 {
                     ErrorPrint(res, "Failed to load step file.\n");
                     break;
                 }
 
-                if ((res = sr.GetResult3DObject(out fl3DObj1, f64ChordalDeviation)).IsFail())
+                if ((res = sr.GetResult3DObject(out fl3DObject2, f64ChordalDeviation)).IsFail())
                 {
                     ErrorPrint(res, "Failed to get 3D object from the StepReader.\n");
                     break;
                 }
 
-                if ((res = view3D[1].PushObject(fl3DObj1)).IsFail())
-                {
-                    ErrorPrint(res, "Failed to display 3D object.\n");
-                    break;
-                }
+				view3D[1].UpdateObject(i32ReturnIndex);
+				view3D[1].ZoomFit();
 
-                // 3D View 에서 카메라 객체 얻어 오기
-                // Gets the camera object from the 3D View.
-                CGUIView3DCamera camera = new CGUIView3DCamera();
+				// 3D View 에서 카메라 객체 얻어 오기
+				// Gets the camera object from the 3D View.
+				CGUIView3DCamera camera = new CGUIView3DCamera();
 
                 // 카메라가 바라보는 방향을 설정
                 // Sets the direction that the camera is looking.
