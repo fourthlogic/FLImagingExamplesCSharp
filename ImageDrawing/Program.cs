2b3a66033577f45a031881ce94920c2d25e1f9b9
ks <ks@fourthlogic.co.kr>
2024-08-22 18:40:42 +0900

    Add ImageDrawing2 Example

--- ImageDrawing/Program.cs
+++ ImageDrawing/Program.cs
@@ -94,126 +94,50 @@ namespace ImageDrawing
 			        break;
 		        }
 
-		        // Blob 객체 생성 // Create Blob object
-				CBlob sBlob = new CBlob();
+                // 레이어는 따로 해제하지 않아도 View가 해제 될 때 같이 해제된다. // The layer is released together when View is released without releasing it separately.
+                CGUIViewImageLayer layerSrc = viewImageSrc.GetLayer(0);
 
-				// 처리할 이미지 설정 // Set the image to process
-				sBlob.SetSourceImage(ref fliImage);
-				// 논리 조건 설정
-				sBlob.SetLogicalCondition(ELogicalCondition.Less);
-				// 임계값 설정,  위의 조건과 아래의 조건이 합쳐지면 127보다 작은 객체를 검출
-				sBlob.SetThreshold(127);
+                // 이미지에 출력하기 위해 이미지 드로잉 객채에서 레이어를 얻어옴 // Gets layers from image drawing object for output to image
+                CGUIViewImageLayer layerDst = fliImageDrawing.GetLayer();
 
-				// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
-				if((res = sBlob.Execute()).IsFail())
-				{
-					ErrorPrint(res, "Failed to execute Blob.");
-					break;
-				}
+                // 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+                layerSrc.Clear();
+                layerDst.Clear();
 
-				// 면적이 500보다 작은 객체들을 제거
-				if((res = sBlob.Filter(CBlob.EFilterItem.Area, 500, ELogicalCondition.LessEqual)).IsFail())
-				{
-					ErrorPrint(res, "Blob filtering algorithm error occurred.");
-					break;
-				}
+                // 이미지에 정보 표시 // Display image information
+		        layerSrc.DrawTextImage(new CFLPoint<double>(0, 0), "Source Image", EColor.YELLOW, EColor.BLACK, 30);
+		        layerDst.DrawTextImage(new CFLPoint<double>(0, 0), "Destination Image", EColor.YELLOW, EColor.BLACK, 30);
 
-				// Blob 결과를 얻어오기 위해 FigureArray 선언
-				CFLFigureArray flfaBoundaryRects;
+		        CFLPoint<double> flpDraw = new CFLPoint<double>(10.0, 10.0);
 
-				// Blob 결과들 중 Boundary Rectangle 을 얻어옴
-				if((res = sBlob.GetResultBoundaryRects(out flfaBoundaryRects)).IsFail())
-				{
-					ErrorPrint(res, "Failed to get boundary rects from the Blob object.");
-					break;
-				}
+		        layerSrc.DrawFigureImage(flpDraw, EColor.RED, 3);
+		        layerDst.DrawFigureImage(flpDraw, EColor.RED, 3);
 
-		        // 화면에 출력하기 위해 Image View에서 레이어를 얻어옴 // Obtain layer from image view for display
-		        // 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
-		        CGUIViewImageLayer layer1 = viewImageSrc.GetLayer(0);
-		        CGUIViewImageLayer layer2 = viewImageSrc.GetLayer(1);
-		        CGUIViewImageLayer layer3 = viewImageSrc.GetLayer(2);
+		        CFLLine<double> fllDraw = new CFLLine<double>(15.0, 15.0, 80.0, 30.0);
 
-		        // 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
-		        layer1.Clear();
-		        layer2.Clear();
-		        layer3.Clear();
+		        layerSrc.DrawFigureImage(fllDraw, EColor.ORANGE, 3);
+		        layerDst.DrawFigureImage(fllDraw, EColor.ORANGE, 3);
 
-		        // 이미지 뷰 정보 표시 // Display image view information
-		        if((res = layer3.DrawTextCanvas(new CFLPoint<double>(0, 0), "Source Image", EColor.YELLOW, EColor.BLACK, 30)).IsFail())
-		        {
-			        ErrorPrint(res, "Failed to draw text\n");
-			        break;
-		        }
+		        CFLRect<double> flrDraw = new CFLRect<double>(80.0, 80.0, 150.0, 150.0);
 
-		        // flfaBoundaryRects 는 Figure들의 배열이기 때문에 Layer에 넣기만 해도 모두 드로윙이 가능하다.
-				// 아래 함수 DrawFigureImage는 Image좌표를 기준으로 하는 Figure를 Drawing 한다는 것을 의미하며 // The function DrawFigureImage below means drawing a picture based on the image coordinates
-				// 맨 마지막 두개의 파라미터는 불투명도 값이고 1일경우 불투명, 0일경우 완전 투명을 의미한다. // The last two parameters are opacity values, which mean opacity for 1 day and complete transparency for 0 day.
-				// 여기서 0.25이므로 옅은 반투명 상태라고 볼 수 있다.
-				// 파라미터 순서 : 레이어 -> Figure 객체 -> 선 색 -> 선 두께 -> 면 색 -> 펜 스타일 -> 선 알파값(불투명도) -> 면 알파값 (불투명도) // Parameter order: Layer -> Figure object -> Line color -> Line thickness -> Face color -> Pen style -> Line alpha value (opacity) -> Area alpha value (opacity)
-				if((res = layer1.DrawFigureImage(flfaBoundaryRects, EColor.RED, 1, EColor.RED, EGUIViewImagePenStyle.Solid, 1.0f, .25f)).IsFail())
-				{
-					ErrorPrint(res, "Failed to draw figure objects on the image view.\n");
-					break;
-				}
+		        layerSrc.DrawFigureImage(flrDraw, EColor.YELLOW, 3);
+		        layerDst.DrawFigureImage(flrDraw, EColor.YELLOW, 3);
 
-				// Rect 정보값을 각각 확인하는 코드
-				for(Int64 i = 0; i < flfaBoundaryRects.GetCount(); ++i)
-				{
-					CFLRect<Int32> flrRect = (CFLRect<Int32>)flfaBoundaryRects.GetAt(i);
+		        CFLQuad<double> flqDraw = new CFLQuad<double>(170.0, 170.0, 200.0, 180.0, 220.0, 210.0, 180.0, 230.0);
 
-					if(flrRect != null)
-					{
-						Console.Write("No. [{0}]\n", i);
-						Console.Write("LeftTop     : ({0},{1})\n", flrRect.left, flrRect.top);
-						Console.Write("RightBottom : ({0},{1})\n", flrRect.right, flrRect.bottom);
-						Console.Write("Width  : {0}\n", flrRect.GetWidth());
-						Console.Write("Height : {0}\n", flrRect.GetHeight());
-						Console.Write("Center : ({0},{1})\n\n", flrRect.GetCenter().x, flrRect.GetCenter().y);
+		        layerSrc.DrawFigureImage(flqDraw, EColor.GREEN, 3);
+		        layerDst.DrawFigureImage(flqDraw, EColor.GREEN, 3);
 
-						string strNumber = string.Format("[{0}]", i);
-						string strLeftTop = string.Format("({0}, {1})", flrRect.left, flrRect.top);
-						string strRightBottom = string.Format("({0}, {1})", flrRect.right, flrRect.bottom);
-						string strInfo = string.Format("Width : {0}\nHeight : {1}\nCenter : ({2}, {3})", flrRect.GetWidth(), flrRect.GetHeight(), flrRect.GetCenter().x, flrRect.GetCenter().y);
+		        CFLCircle<double> flcDraw = new CFLCircle<double>(250.0, 250.0, 50.0);
+  
+  		        layerSrc.DrawFigureImage(flcDraw, EColor.BLUE, 3);
+  		        layerDst.DrawFigureImage(flcDraw, EColor.BLUE, 3);
 
-						CFLPoint<double> flpLeftTop = new CFLPoint<double>(flrRect.left, flrRect.top);
-						CFLPoint<double> flpRightBottom = new CFLPoint<double>(flrRect.right, flrRect.bottom);
-						CFLPoint<double> flpRightTop = new CFLPoint<double>(flrRect.right, flrRect.top);
+                CFLEllipse<double> fleDraw = new CFLEllipse<double>(350.0, 350.0, 50.0, 80.0, 25.0);
 
-						// 아래 함수 DrawTextImage는 Image좌표를 기준으로 하는 Text를 Drawing 한다는 것을 의미한다.
-						// 파라미터 순서 : 레이어 -> 문자열 좌표 -> 문자열 지정 -> 문자열 색 -> 면 색 -> 폰트 크기 -> 실제 크기로 그릴지의 여부 -> 각도 -> 문자열의 위치 기준
-						layer2.DrawTextImage(flrRect.GetCenter(), strNumber, EColor.CYAN, EColor.BLACK, 12, false, 0, EGUIViewImageTextAlignment.CENTER_CENTER);
-						layer2.DrawTextImage(flpLeftTop, strLeftTop, EColor.YELLOW, EColor.BLACK, 12, false, 0, EGUIViewImageTextAlignment.RIGHT_BOTTOM);
-						layer2.DrawTextImage(flpRightBottom, strRightBottom, EColor.YELLOW, EColor.BLACK);
-						layer2.DrawTextImage(flpRightTop, strInfo, EColor.LIME, EColor.BLACK, 12, false, 0, EGUIViewImageTextAlignment.LEFT_BOTTOM);
-					}
-				}
+		        layerSrc.DrawFigureImage(fleDraw, EColor.VIOLET, 3);
+		        layerDst.DrawFigureImage(fleDraw, EColor.VIOLET, 3);
 
-		        // 이미지에 출력하기 위해 이미지 드로잉 객채에서 레이어를 얻어옴 // Gets layers from image drawing object for output to image
-		        CGUIViewImageLayer layer = fliImageDrawing.GetLayer();
-
-		        // 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
-		        layer.Clear();
-
-		        // 기존 레이어에 드로잉된 원소들을 해당 레이어 뒤쪽에 추가합니다. // Add elements drawn from an existing layer to the back of that layer.
-		        if((res = layer.PushBack(layer1)).IsFail())
-		        {
-			        ErrorPrint(res, "Failed to push back.\n");
-			        break;
-		        }
-
-		        if((res = layer.PushBack(layer2)).IsFail())
-		        {
-			        ErrorPrint(res, "Failed to push back.\n");
-			        break;
-		        }
-
-		        // 이미지 뷰 정보 표시 // Display image view information
-		        if((res = layer.DrawTextImage(new CFLPoint<double>(0, 0), "Destination Image", EColor.YELLOW, EColor.BLACK, 30)).IsFail())
-		        {
-			        ErrorPrint(res, "Failed to draw text\n");
-			        break;
-		        }
 
 		        // 이미지에 그립니다. // Draw in the image.
 		        if((res = fliImageDrawing.Draw()).IsFail())
---
+++ ImageDrawing2/Program.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using FLImagingCLR;
+using FLImagingCLR.Base;
+using FLImagingCLR.Foundation;
+using FLImagingCLR.GUI;
+using FLImagingCLR.ImageProcessing;
+using FLImagingCLR.AdvancedFunctions;
+
+using CResult = FLImagingCLR.CResult;
+
+namespace ImageDrawing2
+{
+    class Program
+    {
+        public static void ErrorPrint(CResult cResult, string str)
+        {
+            if (str.Length > 1)
+                Console.WriteLine(str);
+
+            Console.WriteLine("Error code : {0}\nError name : {1}\n", cResult.GetResultCode(), cResult.GetString());
+            Console.WriteLine("\n");
+            Console.ReadKey();
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+		{
+			// 이미지 객체 선언 // Declare image object
+	        CFLImage fliImage = new CFLImage();
+
+	        // 이미지 드로잉 객체 선언 // Declare image drawing object
+	        CFLImageDrawing fliImageDrawing = new CFLImageDrawing();
+
+	        // 이미지 뷰 선언 // Declare image view
+	        CGUIViewImage viewImageSrc = new CGUIViewImage();
+	        CGUIViewImage viewImageDst = new CGUIViewImage();
+
+	        do
+	        {
+                // 수행 결과 객체 선언 // Declare the execution result object
+			    CResult res;
+
+		        // 이미지 로드 // Load image
+		        if((res = fliImage.Load("../../ExampleImages/Blob/AlignBall.flif")).IsFail())
+		        {
+			        ErrorPrint(res, "Failed to load the image file.\n");
+			        break;
+		        }
+
+		        // Drawing 이미지를 Src 이미지와 동일한 이미지로 생성
+		        if((res = fliImageDrawing.Assign(fliImage)).IsFail())
+		        {
+			        ErrorPrint(res, "Failed to assign the image file.\n");
+			        break;
+		        }
+
+		        // 이미지 뷰 생성 // Create image view
+		        if((res = viewImageSrc.Create(400, 0, 800, 400)).IsFail())
+		        {
+			        ErrorPrint(res, "Failed to create the image view.\n");
+			        break;
+		        }
+
+		        // 이미지 뷰 생성 // Create image view
+		        if((res = viewImageDst.Create(800, 0, 1200, 400)).IsFail())
+		        {
+			        ErrorPrint(res, "Failed to create the image view.\n");
+			        break;
+		        }
+
+		        // 두 이미지 뷰의 시점을 동기화 한다. // Synchronize the viewpoints of the two image views.
+		        if((res = viewImageSrc.SynchronizePointOfView(ref viewImageDst)).IsFail())
+		        {
+			        ErrorPrint(res, "Failed to synchronize view\n");
+			        break;
+		        }
+
+		        // 두 이미지 뷰 윈도우의 위치를 동기화 한다 // Synchronize the positions of the two image view windows
+		        if((res = viewImageSrc.SynchronizeWindow(ref viewImageDst)).IsFail())
+		        {
+			        ErrorPrint(res, "Failed to synchronize window.\n");
+			        break;
+		        }
+
+		        // 이미지 뷰에 이미지를 디스플레이 // Display an image in an image view
+		        if((res = viewImageSrc.SetImagePtr(ref fliImage)).IsFail())
+		        {
+			        ErrorPrint(res, "Failed to set image object on the image view.\n");
+			        break;
+		        }
+
+		        // Blob 객체 생성 // Create Blob object
+				CBlob sBlob = new CBlob();
+
+				// 처리할 이미지 설정 // Set the image to process
+				sBlob.SetSourceImage(ref fliImage);
+				// 논리 조건 설정
+				sBlob.SetLogicalCondition(ELogicalCondition.Less);
+				// 임계값 설정,  위의 조건과 아래의 조건이 합쳐지면 127보다 작은 객체를 검출
+				sBlob.SetThreshold(127);
+
+				// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+				if((res = sBlob.Execute()).IsFail())
+				{
+					ErrorPrint(res, "Failed to execute Blob.");
+					break;
+				}
+
+				// 면적이 500보다 작은 객체들을 제거
+				if((res = sBlob.Filter(CBlob.EFilterItem.Area, 500, ELogicalCondition.LessEqual)).IsFail())
+				{
+					ErrorPrint(res, "Blob filtering algorithm error occurred.");
+					break;
+				}
+
+				// Blob 결과를 얻어오기 위해 FigureArray 선언
+				CFLFigureArray flfaBoundaryRects;
+
+				// Blob 결과들 중 Boundary Rectangle 을 얻어옴
+				if((res = sBlob.GetResultBoundaryRects(out flfaBoundaryRects)).IsFail())
+				{
+					ErrorPrint(res, "Failed to get boundary rects from the Blob object.");
+					break;
+				}
+
+		        // 화면에 출력하기 위해 Image View에서 레이어를 얻어옴 // Obtain layer from image view for display
+		        // 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+		        CGUIViewImageLayer layer1 = viewImageSrc.GetLayer(0);
+		        CGUIViewImageLayer layer2 = viewImageSrc.GetLayer(1);
+		        CGUIViewImageLayer layer3 = viewImageSrc.GetLayer(2);
+
+		        // 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+		        layer1.Clear();
+		        layer2.Clear();
+		        layer3.Clear();
+
+		        // 이미지 뷰 정보 표시 // Display image view information
+		        if((res = layer3.DrawTextCanvas(new CFLPoint<double>(0, 0), "Source Image", EColor.YELLOW, EColor.BLACK, 30)).IsFail())
+		        {
+			        ErrorPrint(res, "Failed to draw text\n");
+			        break;
+		        }
+
+		        // flfaBoundaryRects 는 Figure들의 배열이기 때문에 Layer에 넣기만 해도 모두 드로윙이 가능하다.
+				// 아래 함수 DrawFigureImage는 Image좌표를 기준으로 하는 Figure를 Drawing 한다는 것을 의미하며 // The function DrawFigureImage below means drawing a picture based on the image coordinates
+				// 맨 마지막 두개의 파라미터는 불투명도 값이고 1일경우 불투명, 0일경우 완전 투명을 의미한다. // The last two parameters are opacity values, which mean opacity for 1 day and complete transparency for 0 day.
+				// 여기서 0.25이므로 옅은 반투명 상태라고 볼 수 있다.
+				// 파라미터 순서 : 레이어 -> Figure 객체 -> 선 색 -> 선 두께 -> 면 색 -> 펜 스타일 -> 선 알파값(불투명도) -> 면 알파값 (불투명도) // Parameter order: Layer -> Figure object -> Line color -> Line thickness -> Face color -> Pen style -> Line alpha value (opacity) -> Area alpha value (opacity)
+				if((res = layer1.DrawFigureImage(flfaBoundaryRects, EColor.RED, 1, EColor.RED, EGUIViewImagePenStyle.Solid, 1.0f, .25f)).IsFail())
+				{
+					ErrorPrint(res, "Failed to draw figure objects on the image view.\n");
+					break;
+				}
+
+				// Rect 정보값을 각각 확인하는 코드
+				for(Int64 i = 0; i < flfaBoundaryRects.GetCount(); ++i)
+				{
+					CFLRect<Int32> flrRect = (CFLRect<Int32>)flfaBoundaryRects.GetAt(i);
+
+					if(flrRect != null)
+					{
+						Console.Write("No. [{0}]\n", i);
+						Console.Write("LeftTop     : ({0},{1})\n", flrRect.left, flrRect.top);
+						Console.Write("RightBottom : ({0},{1})\n", flrRect.right, flrRect.bottom);
+						Console.Write("Width  : {0}\n", flrRect.GetWidth());
+						Console.Write("Height : {0}\n", flrRect.GetHeight());
+						Console.Write("Center : ({0},{1})\n\n", flrRect.GetCenter().x, flrRect.GetCenter().y);
+
+						string strNumber = string.Format("[{0}]", i);
+						string strLeftTop = string.Format("({0}, {1})", flrRect.left, flrRect.top);
+						string strRightBottom = string.Format("({0}, {1})", flrRect.right, flrRect.bottom);
+						string strInfo = string.Format("Width : {0}\nHeight : {1}\nCenter : ({2}, {3})", flrRect.GetWidth(), flrRect.GetHeight(), flrRect.GetCenter().x, flrRect.GetCenter().y);
+
+						CFLPoint<double> flpLeftTop = new CFLPoint<double>(flrRect.left, flrRect.top);
+						CFLPoint<double> flpRightBottom = new CFLPoint<double>(flrRect.right, flrRect.bottom);
+						CFLPoint<double> flpRightTop = new CFLPoint<double>(flrRect.right, flrRect.top);
+
+						// 아래 함수 DrawTextImage는 Image좌표를 기준으로 하는 Text를 Drawing 한다는 것을 의미한다.
+						// 파라미터 순서 : 레이어 -> 문자열 좌표 -> 문자열 지정 -> 문자열 색 -> 면 색 -> 폰트 크기 -> 실제 크기로 그릴지의 여부 -> 각도 -> 문자열의 위치 기준
+						layer2.DrawTextImage(flrRect.GetCenter(), strNumber, EColor.CYAN, EColor.BLACK, 12, false, 0, EGUIViewImageTextAlignment.CENTER_CENTER);
+						layer2.DrawTextImage(flpLeftTop, strLeftTop, EColor.YELLOW, EColor.BLACK, 12, false, 0, EGUIViewImageTextAlignment.RIGHT_BOTTOM);
+						layer2.DrawTextImage(flpRightBottom, strRightBottom, EColor.YELLOW, EColor.BLACK);
+						layer2.DrawTextImage(flpRightTop, strInfo, EColor.LIME, EColor.BLACK, 12, false, 0, EGUIViewImageTextAlignment.LEFT_BOTTOM);
+					}
+				}
+
+		        // 이미지에 출력하기 위해 이미지 드로잉 객채에서 레이어를 얻어옴 // Gets layers from image drawing object for output to image
+		        CGUIViewImageLayer layer = fliImageDrawing.GetLayer();
+
+		        // 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+		        layer.Clear();
+
+		        // 기존 레이어에 드로잉된 원소들을 해당 레이어 뒤쪽에 추가합니다. // Add elements drawn from an existing layer to the back of that layer.
+		        if((res = layer.PushBack(layer1)).IsFail())
+		        {
+			        ErrorPrint(res, "Failed to push back.\n");
+			        break;
+		        }
+
+		        if((res = layer.PushBack(layer2)).IsFail())
+		        {
+			        ErrorPrint(res, "Failed to push back.\n");
+			        break;
+		        }
+
+		        // 이미지 뷰 정보 표시 // Display image view information
+		        if((res = layer.DrawTextImage(new CFLPoint<double>(0, 0), "Destination Image", EColor.YELLOW, EColor.BLACK, 30)).IsFail())
+		        {
+			        ErrorPrint(res, "Failed to draw text\n");
+			        break;
+		        }
+
+		        // 이미지에 그립니다. // Draw in the image.
+		        if((res = fliImageDrawing.Draw()).IsFail())
+		        {
+			        ErrorPrint(res, "Failed to draw.\n");
+			        break;
+		        }
+
+		        // 이미지 뷰에 이미지를 디스플레이 // Display an image in an image view
+                CFLImage fliTemp = (CFLImage)fliImageDrawing;
+                if ((res = viewImageDst.SetImagePtr(ref fliTemp)).IsFail())
+		        {
+			        ErrorPrint(res, "Failed to set image object on the image view.\n");
+			        break;
+		        }
+
+		        // 이미지 뷰를 갱신 합니다. // Update image view
+		        viewImageSrc.Invalidate();
+		        viewImageDst.Invalidate();
+
+		        // 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
+		        while(viewImageSrc.IsAvailable() || viewImageDst.IsAvailable())
+			        CThreadUtilities.Sleep(1);
+	        }
+	        while(false);
+		}
+    }
+}
