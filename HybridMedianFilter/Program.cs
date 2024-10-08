3ddd61e4ef6670b95e0e47c7ddaa3ce7362d35c6
hkjeong <hkjeong@fourthlogic.co.kr>
2024-10-08 16:39:53 +0900

    Add Hybrid Median Filter Example

---
+++ HybridMedianFilter/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using FLImagingCLR;
+using FLImagingCLR.Base;
+using FLImagingCLR.Foundation;
+using FLImagingCLR.GUI;
+using FLImagingCLR.ImageProcessing;
+using FLImagingCLR.AdvancedFunctions;
+
+namespace HybridMedianFilter
+{
+	class Program
+	{
+		public static void ErrorPrint(CResult cResult, string str)
+		{
+			if (str.Length > 1)
+				Console.WriteLine(str);
+
+			Console.WriteLine("Error code : {0}\nError name : {1}\n", cResult.GetResultCode(), cResult.GetString());
+			Console.WriteLine("\n");
+			Console.ReadKey();
+		}
+
+		[STAThread]
+		static void Main(string[] args)
+		{
+			// 이미지 객체 선언 // Declare the image object
+			CFLImage fliSrcImage = new CFLImage();
+			CFLImage fliDstImage = new CFLImage();
+
+			// 이미지 뷰 선언 // Declare the image view
+			CGUIViewImage viewImageSrc = new CGUIViewImage();
+			CGUIViewImage viewImageDst = new CGUIViewImage();
+
+			do
+			{
+				// 알고리즘 동작 결과 // Algorithm execution result
+				CResult res = new CResult();
+
+				// 이미지 로드 // Load image
+				if ((res =fliSrcImage.Load("../../ExampleImages/MedianFilter/Chip_Noise.flif")).IsFail())
+				{
+					ErrorPrint(res, "Failed to load the image file.\n");
+					break;
+				}
+
+				// Destination이미지를 Src 이미지와 동일한 이미지로 생성
+				if ((res =fliDstImage.Assign(fliSrcImage)).IsFail())
+				{
+					ErrorPrint(res, "Failed to assign the image file.\n");
+					break;
+				}
+
+				// 이미지 뷰 생성 // Create image view
+				if ((res =viewImageSrc.Create(400, 0, 912, 612)).IsFail())
+				{
+					ErrorPrint(res, "Failed to create the image view.\n");
+					break;
+				}
+
+				// 이미지 뷰에 이미지를 디스플레이 // Display an image in an image view
+				if ((res =viewImageSrc.SetImagePtr(ref fliSrcImage)).IsFail())
+				{
+					ErrorPrint(res, "Failed to set image object on the image view.\n");
+					break;
+				}
+
+				// Destination 이미지 뷰 생성 // Create the destination image view
+				if ((res =viewImageDst.Create(912, 0, 1424, 612)).IsFail())
+				{
+					ErrorPrint(res, "Failed to create the image view.\n");
+					break;
+				}
+
+				// Destination 이미지 뷰에 이미지를 디스플레이 // Display the image in the destination image view
+				if ((res =viewImageDst.SetImagePtr(ref fliDstImage)).IsFail())
+				{
+					ErrorPrint(res, "Failed to set image object on the image view.\n");
+					break;
+				}
+
+				// Source이미지 뷰와 Dst 이미지 뷰의 초점을 맞춤
+				if ((res =viewImageSrc.SynchronizePointOfView(ref viewImageDst)).IsFail())
+				{
+					ErrorPrint(res, "Failed to set image object on the image view.\n");
+					break;
+				}
+
+				// Image 크기에 맞게 view의 크기를 조정 // Zoom the view to fit the image size
+				if ((res =viewImageSrc.ZoomFit()).IsFail())
+				{
+					ErrorPrint(res, "Failed to zoom fit\n");
+					break;
+				}
+
+				// Image 크기에 맞게 view의 크기를 조정 // Zoom the view to fit the image size
+				if ((res =viewImageDst.ZoomFit()).IsFail())
+				{
+					ErrorPrint(res, "Failed to zoom fit\n");
+					break;
+				}
+
+				// Blob 객체 생성 // Create Blob object
+				CHybridMedianFilter hybridMedianFilter = new CHybridMedianFilter();
+
+				// 처리할 이미지 설정 // Set the image to process
+				hybridMedianFilter.SetSourceImage(ref fliSrcImage);
+
+				// ROI 범위 설정
+				CFLRect<Int32> flrROI = new CFLRect<int>(100, 190, 360, 420);
+
+				// Source ROI 설정 // Set the Source ROI
+				hybridMedianFilter.SetSourceROI(flrROI);
+
+				// Destination 이미지 설정 // Set the destination image
+				hybridMedianFilter.SetDestinationImage(ref fliDstImage);
+
+				// Destination ROI 설정 // Set Destination ROI
+				hybridMedianFilter.SetDestinationROI(flrROI);
+
+				// 처리할 hybridMedianFilter의 Kernel Size 설정 (KernelSize = 11 일 경우, Kernel Size : 11x11, 홀수만 설정가능)
+				hybridMedianFilter.SetKernel(11);
+
+				// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+				if ((res =(res = hybridMedianFilter.Execute())).IsFail())
+				{
+					ErrorPrint(res, "Failed to execute Hybrid Median Filter.");
+					break;
+				}
+
+				// 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
+				// 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+				CGUIViewImageLayer layerSrc = viewImageSrc.GetLayer(0);
+				CGUIViewImageLayer layerDst = viewImageDst.GetLayer(0);
+
+				// 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+				layerSrc.Clear();
+				layerDst.Clear();
+
+				// ROI영역이 어디인지 알기 위해 디스플레이 한다 // Display to find out where ROI is
+				// FLImaging의 Figure객체들은 어떤 도형모양이든 상관없이 하나의 함수로 디스플레이가 가능
+				if ((res =layerSrc.DrawFigureImage(flrROI, EColor.BLUE)).IsFail())
+				{
+					ErrorPrint(res, "Failed to draw figures objects on the image view.\n");
+					break;
+				}
+
+				if ((res =layerDst.DrawFigureImage(flrROI, EColor.BLUE)).IsFail())
+				{
+					ErrorPrint(res, "Failed to draw figures objects on the image view.\n");
+					break;
+				}
+
+				CFLPoint<double> flp = new CFLPoint<double>();
+
+				if ((res =layerSrc.DrawTextCanvas(flp, ("Source Image"), EColor.YELLOW, EColor.BLACK, 20)).IsFail())
+				{
+					ErrorPrint(res, "Failed to draw text\n");
+					break;
+				}
+
+				if ((res =layerDst.DrawTextCanvas(flp, ("Destination Image"), EColor.YELLOW, EColor.BLACK, 20)).IsFail())
+				{
+					ErrorPrint(res, "Failed to draw text\n");
+					break;
+				}
+
+				// 이미지 뷰를 갱신 합니다. // Update image view
+				viewImageSrc.Invalidate(true);
+				viewImageDst.Invalidate(true);
+
+				// 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
+				while (viewImageSrc.IsAvailable() && viewImageDst.IsAvailable())
+					Thread.Sleep(1);
+			}
+			while (false);
+		}
+	}
+}
