b1600ae412f054c926ce37636dcfd3e69d5344c9
sbno <sbno@fourthlogic.co.kr>
2025-01-16 08:44:59 +0900

    Add StringBasedOCR Example

---
+++ StringBasedOCRDL/Program.cs
@@ -0,0 +1,328 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Net.NetworkInformation;
+
+using FLImagingCLR;
+using FLImagingCLR.Base;
+using FLImagingCLR.Foundation;
+using FLImagingCLR.GUI;
+using FLImagingCLR.ImageProcessing;
+using FLImagingCLR.AdvancedFunctions;
+using FLImagingCLR.ThreeDim;
+using FLImagingCLR.AI;
+
+namespace StringBasedOCRDL
+{
+	class Program
+	{
+		public static void ErrorPrint(CResult cResult, string str)
+		{
+			if(str.Length > 1)
+				Console.WriteLine(str);
+
+			Console.WriteLine("Error code : {0}\nError name : {1}\n", cResult.GetResultCode(), cResult.GetString());
+			Console.WriteLine("\n");
+			Console.ReadKey();
+		}
+
+		[STAThread]
+		static void Main(string[] args)
+		{
+			// 이미지 객체 선언 // Declare the image object
+			CFLImage fliLearnImage = new CFLImage();
+			CFLImage fliValidationImage = new CFLImage();
+
+			/// 이미지 뷰 선언 // Declare the image view
+			CGUIViewImage viewImageLearn = new CGUIViewImage();
+			CGUIViewImage viewImageValidation = new CGUIViewImage();
+
+			// 그래프 뷰 선언 // Declare the graph view
+			CGUIViewGraph viewGraph = new CGUIViewGraph();
+			bool bTerminated = false;
+
+			CResult res = new CResult();
+
+			do
+			{
+				// 라이브러리가 완전히 로드 될 때까지 기다림 // Wait for the library to fully load
+				Thread.Sleep(1000);
+
+				// 이미지 로드 // Load image
+				if((res = fliLearnImage.Load("../../ExampleImages/StringBasedOCRDL/Learn.flif")).IsFail())
+				{
+					ErrorPrint(res, "Failed to load the image file. \n");
+					break;
+				}
+
+				if((res = fliValidationImage.Load("../../ExampleImages/StringBasedOCRDL/Source.flif")).IsFail())
+				{
+					ErrorPrint(res, "Failed to load the image file.\n");
+					break;
+				}
+
+				// 이미지 뷰 생성 // Create image view
+				if((res = viewImageLearn.Create(100, 0, 600, 500)).IsFail())
+				{
+					ErrorPrint(res, "Failed to create the image view. \n");
+					break;
+				}
+
+				if((res = viewImageValidation.Create(600, 0, 1100, 500)).IsFail())
+				{
+					ErrorPrint(res, "Failed to create the image view.\n");
+					break;
+				}
+
+				// Graph 뷰 생성 // Create graph view
+				if((res = viewGraph.Create(600, 500, 1100, 1000)).IsFail())
+				{
+					ErrorPrint(res, " Failed to create the graph view. \n");
+					break;
+				}
+
+				viewGraph.SetDarkMode();
+
+				// 이미지 뷰에 이미지를 디스플레이 // display the image in the imageview
+				if((res = viewImageLearn.SetImagePtr(ref fliLearnImage)).IsFail())
+				{
+					ErrorPrint(res, "Failed to set image object on the image view. \n");
+					break;
+				}
+
+				if((res = viewImageValidation.SetImagePtr(ref fliValidationImage)).IsFail())
+				{
+					ErrorPrint(res, "Failed to set image object on the image view. \n");
+					break;
+				}
+
+				// 다섯 개의 이미지 뷰 윈도우의 위치를 동기화 한다 // Synchronize the positions of the four image view windows
+				if((res = viewImageLearn.SynchronizeWindow(ref viewImageValidation)).IsFail())
+				{
+					ErrorPrint(res, "Failed to synchronize window. \n");
+					break;
+				}
+
+				// 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
+				// 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+				CGUIViewImageLayer layerLearn = viewImageLearn.GetLayer(0);
+				CGUIViewImageLayer layerValidation = viewImageValidation.GetLayer(0);
+
+				// 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+				layerLearn.Clear();
+				layerValidation.Clear();
+
+				// View 정보를 디스플레이 합니다. // Display View information.
+				// 아래 함수 DrawTextCanvas은 Screen좌표를 기준으로 하는 String을 Drawing 한다.// The function DrawTextCanvas below draws a String based on the screen coordinates.
+				// 파라미터 순서 : 레이어 -> 기준 좌표 Figure 객체 -> 문자열 -> 폰트 색 -> 면 색 -> 폰트 크기 -> 실제 크기 유무 -> 각도 ->
+				//                 얼라인 -> 폰트 이름 -> 폰트 알파값(불투명도) -> 면 알파값 (불투명도) -> 폰트 두께 -> 폰트 이텔릭
+				// Parameter order: layer -> reference coordinate Figure object -> string -> font color -> Area color -> font size -> actual size -> angle ->
+				//                  Align -> Font Name -> Font Alpha Value (Opaqueness) -> Cotton Alpha Value (Opaqueness) -> Font Thickness -> Font Italic
+				CFLPoint<double> flpPoint = new CFLPoint<double>(0, 0);
+
+				if((res = layerLearn.DrawTextCanvas(flpPoint, "LEARN", EColor.YELLOW, EColor.BLACK, 30)).IsFail())
+				{
+					ErrorPrint(res, "Failed to draw text\n");
+					break;
+				}
+
+				if((res = layerValidation.DrawTextCanvas(flpPoint, "VALIDATION", EColor.YELLOW, EColor.BLACK, 30)).IsFail())
+				{
+					ErrorPrint(res, "Failed to draw text\n");
+					break;
+				}
+
+				// 이미지 뷰를 갱신 // Update the image view.
+				viewImageLearn.RedrawWindow();
+				viewImageValidation.RedrawWindow();
+
+				// OCR 객체 생성 // Create OCR object
+				CStringBasedOCRDL ocr = new CStringBasedOCRDL();
+
+				// 학습할 이미지 설정 // Set the image to learn
+				ocr.SetLearningImage(ref fliLearnImage);
+				// 검증할 이미지 설정 // Set the image to validate
+				ocr.SetLearningValidationImage(ref fliValidationImage);
+				// 분류할 이미지 설정 // Set the image to classify
+				ocr.SetInferenceImage(ref fliValidationImage);
+				ocr.SetInferenceResultImage(ref fliValidationImage);
+
+				// 학습할 OCR 모델 설정 // Set up the OCR model to learn
+				ocr.SetModel(CStringBasedOCRDL.EModel.FLNet);
+				// 학습할 OCR 모델 Version 설정 // Set up the OCR model version to learn
+				ocr.SetModelVersion(CStringBasedOCRDL.EModelVersion.FLNet_V1_32_256);
+				// 학습 epoch 값을 설정 // Set the learn epoch value 
+				ocr.SetLearningEpoch(500);
+				// 학습 이미지 Interpolation 방식 설정 // Set Interpolation method of learn image
+				ocr.SetInterpolationMethod(EInterpolationMethod.Bilinear);
+
+				// OptimizerSpec 객체 생성 // Create OptimizerSpec object
+				COptimizerSpecAdamGradientDescent optSpec = new COptimizerSpecAdamGradientDescent();
+
+				// Optimizer의 학습률 설정 // Set learning rate of Optimizer
+				optSpec.SetLearningRate(.001f);
+
+				// 설정한 Optimizer를 OCR에 적용 // Apply Optimizer that we set up to OCR
+				ocr.SetLearningOptimizerSpec(optSpec);
+
+				// AugmentationSpec 설정 // Set the AugmentationSpec
+				CAugmentationSpec augSpec = new CAugmentationSpec();
+
+				augSpec.SetCommonActivationRatio(0.5);
+				augSpec.SetCommonIoUThreshold(0.8);
+				augSpec.EnableRotation(true);
+				augSpec.SetRotationParam(10.0, false, false);
+
+				augSpec.EnableScale(true);
+				augSpec.SetScaleParam(.8, 1.2, .8, 1.2, false);
+
+				augSpec.EnableTranslation(true);
+				augSpec.SetTranslationParam(.1, .1);
+
+				ocr.SetLearningAugmentationSpec(augSpec);
+
+				// 학습을 종료할 조건식 설정. Validation 값이 1.0 이상인 경우 학습 종료한다.
+				// Set Conditional Expression to End Learning. If the validation value is 1.0 or higher, end the learning.
+				ocr.SetLearningStopCondition("metric >= 1 & map >= 1");
+
+				// 자동 저장 옵션 설정 // Set Auto-Save Options
+				CAutoSaveSpec autoSaveSpec = new CAutoSaveSpec();
+
+				// 자동 저장 활성화 // Enable Auto-Save
+				autoSaveSpec.EnableAutoSave(true);
+				// 저장할 모델 경로 설정 // Set Model path to save
+				autoSaveSpec.SetAutoSavePath("model.flsbocrdl");
+				// 자동 저장 조건식 설정. 현재 1-NED 값이 최대 값인 경우 저장 활성화
+				// Set auto-save conditional expressions. Enable save if the current 1-NED value is the maximum value
+				autoSaveSpec.SetAutoSaveCondition("epoch >= 10 & metric > max('metric')");
+
+				// 자동 저장 옵션 설정 // Set Auto-Save Options
+				ocr.SetLearningAutoSaveSpec(autoSaveSpec);
+
+				// OCR learn function을 진행하는 스레드 생성 // Create the OCR Learn function thread
+				ThreadPool.QueueUserWorkItem((arg) =>
+				{
+					if((res = ocr.Learn()).IsFail())
+						ErrorPrint(res, "Failed to execute Learn.\n");
+
+					bTerminated = true;
+				}, null);
+
+				bool bEscape = false;
+
+				ThreadPool.QueueUserWorkItem((arg) =>
+				{
+					if(Console.ReadKey().Key == ConsoleKey.Escape)
+						bEscape = true;
+				}, null);
+
+				while(!ocr.IsRunning() && !bTerminated)
+					Thread.Sleep(1);
+
+				int i32MaxEpoch = ocr.GetLearningEpoch();
+				int i32PrevEpoch = 0;
+				int i32PrevCostCount = 0;
+				int i32PrevValidationCount = 0;
+
+				while(true)
+				{
+					Thread.Sleep(1);
+
+					// 마지막 미니 배치 반복 횟수 받기 // Get the last maximum number of iterations of the last mini batch 
+					int i32MiniBatchCount = ocr.GetActualMiniBatchCount();
+					// 마지막 미니 배치 반복 횟수 받기 // Get the last number of mini batch iterations
+					int i32Iteration = ocr.GetLearningResultCurrentIteration();
+					// 마지막 학습 횟수 받기 // Get the last epoch learning
+					int i32Epoch = ocr.GetLastEpoch();
+
+					// 미니 배치 반복이 완료되면 cost와 validation 값을 디스플레이 
+					// Display cost and validation value if iterations of the mini batch is completed 
+					if(i32Epoch != i32PrevEpoch && i32Iteration == i32MiniBatchCount && i32Epoch > 0)
+					{
+						// 마지막 학습 결과 비용 받기 // Get the last cost of the learning result
+						float f32CurrCost = ocr.GetLearningResultLastCost();
+						// 마지막 1-NED 결과 받기 // Get the last validation result
+						float f32Validation1MNED = ocr.GetLearningResultLast1MNED();
+						// 마지막 mAP 결과 받기 // Get the last validation result
+						float f32ValidationMeanAP = ocr.GetLearningResultLastMeanAP();
+
+						// 해당 epoch의 비용과 검증 결과 값 출력 // Print cost and validation value for the relevant epoch
+						Console.WriteLine("Cost : {0:F6} 1-NED : {1:F6} mAP : {2:F6} Epoch {3} / {4}", f32CurrCost, f32Validation1MNED, f32ValidationMeanAP, i32Epoch, i32MaxEpoch);
+
+						// 학습 결과 비용과 검증 결과 기록을 받아 그래프 뷰에 출력  
+						// Get the history of cost and validation and print it at graph view
+						List<float> vctCosts = new List<float>();
+						List<float> vct1MNED = new List<float>();
+						List<float> vctMeanAP = new List<float>();
+						List<int> vctValidationEpoch = new List<int>();
+
+						ocr.GetLearningResultAllHistory(out vctCosts, out vct1MNED, out vctMeanAP, out vctValidationEpoch);
+
+						// 비용 기록이나 검증 결과 기록이 있다면 출력 // Print results if cost or validation history exists
+						if((vctCosts.Count() != 0 && i32PrevCostCount != vctCosts.Count()) || (vct1MNED.Count() != 0 && i32PrevValidationCount != vct1MNED.Count()) || (vctMeanAP.Count() != 0 && i32PrevValidationCount != vctMeanAP.Count()))
+						{
+							int i32Step = ocr.GetLearningValidationStep();
+							List<float> flaX = new List<float>();
+
+							for(long i = 0; i < vctMeanAP.Count() - 1; ++i)
+								flaX.Add((float)(i * i32Step));
+
+							flaX.Add((float)(vctCosts.Count() - 1));
+
+							// 이전 그래프의 데이터를 삭제 // Clear previous grpah data
+							viewGraph.LockUpdate();
+							viewGraph.Clear();
+
+							// Graph View 데이터 입력 // Input Graph View Data
+							viewGraph.Plot(vctCosts, EChartType.Line, EColor.RED, "Cost");
+							// Graph View 데이터 입력 // Input Graph View Data
+							viewGraph.Plot(flaX, vct1MNED, EChartType.Line, EColor.BLUE, "1-NED");
+							// Graph View 데이터 입력 // Input Graph View Data
+							viewGraph.Plot(flaX, vctMeanAP, EChartType.Line, EColor.PINK, "mAP");
+							viewGraph.UnlockUpdate();
+
+							viewGraph.UpdateWindow();
+							viewGraph.Invalidate();
+							viewGraph.RedrawWindow();
+						}
+
+						// 검증 결과가 1.0 이상 일 경우 학습을 중단하고 인식 진행 
+						// If the validation result is greater than 1.0, stop learning and recognize character
+						if(f32Validation1MNED >= 1.0f && f32ValidationMeanAP >= 1.0f || bEscape)
+							ocr.Stop();
+
+						i32PrevEpoch = i32Epoch;
+						i32PrevCostCount = vctCosts.Count();
+						i32PrevValidationCount = vct1MNED.Count();
+					}
+
+					// epoch만큼 학습이 완료되면 종료 // End when learning progresses as much as epoch
+					if(!ocr.IsRunning())
+						break;
+				}
+
+				// 알고리즘 수행 // Execute the algorithm
+				if((res = ocr.Execute()).IsFail())
+				{
+					ErrorPrint(res, "Failed to execute.\n");
+					break;
+				}
+				// 이미지 뷰를 갱신 // Update the image view.
+				viewImageLearn.RedrawWindow();
+				viewImageValidation.RedrawWindow();
+
+				// 그래프 뷰를 갱신 // Update the Graph view.
+				viewGraph.RedrawWindow();
+
+				// 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
+				while(viewImageLearn.IsAvailable() && viewImageValidation.IsAvailable() && viewGraph.IsAvailable())
+					Thread.Sleep(1);
+			}
+			while(false);
+		}
+	}
+}
