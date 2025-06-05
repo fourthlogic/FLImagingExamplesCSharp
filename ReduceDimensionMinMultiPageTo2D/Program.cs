c41c9e282922df504d19fd3210f465a4413d67a3
swbang <swbang@fourthlogic.co.kr>
2025-06-05 12:21:47 +0900

    Add Reduce Dimension Min Multi Page To 2D Example

---
+++ ReduceDimensionMinMultiPageTo2D/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using FLImagingCLR;
+using FLImagingCLR.Base;
+using FLImagingCLR.Foundation;
+using FLImagingCLR.GUI;
+using FLImagingCLR.ImageProcessing;
+using FLImagingCLR.AdvancedFunctions;
+using CResult = FLImagingCLR.CResult;
+
+namespace ReduceDimensionMinMultiPageTo2D
+{
+	class Program
+	{
+		public static void ErrorPrint(CResult cResult, string str)
+		{
+			if(str.Length > 1)
+				Console.WriteLine(str);
+
+			Console.WriteLine("Error code : {0}\nError name : {1}\n", cResult.GetResultCode(), cResult.GetString());
+			Console.WriteLine("\n");
+			Console.ReadKey();
+		}
+
+		[STAThread]
+		static void Main(string[] args)
+		{
+			// 이미지 객체 선언 // Declare the image object
+			CFLImage fliSourceImage = new CFLImage();
+			CFLImage fliDestinationImage = new CFLImage();
+
+			// 이미지 뷰 선언 // Declare the image view
+			CGUIViewImage viewImageSource = new CGUIViewImage();
+			CGUIViewImage viewImageDestination = new CGUIViewImage();
+
+			do
+			{
+				CResult res;
+				// 이미지 로드 // Load image
+				if((res = fliSourceImage.Load("../../ExampleImages/ReduceDimensionMinMultiPageTo2D/Source.flif")).IsFail())
+				{
+					ErrorPrint(res, "Failed to load the image file.\n");
+					break;
+				}
+
+				// 이미지 뷰 생성 // Create image view
+				if((res = viewImageSource.Create(100, 0, 612, 512)).IsFail())
+				{
+					ErrorPrint(res, "Failed to create the image view.\n");
+					break;
+				}
+
+				// 이미지 뷰 생성 // Create image view
+				if((res = viewImageDestination.Create(612, 0, 1124, 512)).IsFail())
+				{
+					ErrorPrint(res, "Failed to create the image view.\n");
+					break;
+				}
+
+				// 두 이미지 뷰 윈도우의 위치를 동기화 한다 // Synchronize the positions of the two image view windows
+				if((res = viewImageSource.SynchronizeWindow(ref viewImageDestination)).IsFail())
+				{
+					ErrorPrint(res, "Failed to synchronize window. \n");
+					break;
+				}
+
+				// 두 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the two image views. .
+				if((res = viewImageSource.SynchronizePointOfView(ref viewImageDestination)).IsFail())
+				{
+					ErrorPrint(res, "Failed to synchronize view. \n");
+					break;
+				}
+
+				// 이미지 뷰에 이미지를 디스플레이 // Display the image in the image view
+				if((res = viewImageSource.SetImagePtr(ref fliSourceImage)).IsFail())
+				{
+					ErrorPrint(res, "Failed to set image object on the image view. \n");
+					break;
+				}
+
+				// 이미지 뷰에 이미지를 디스플레이 // Display the image in the image view
+				if((res = viewImageDestination.SetImagePtr(ref fliDestinationImage)).IsFail())
+				{
+					ErrorPrint(res, "Failed to set image object on the image view. \n");
+					break;
+				}
+
+				// Reduce Dimension Min Multi Page To 2D 객체 생성 // Create Reduce Dimension Min Multi Page To 2D object
+				CReduceDimensionMinMultiPageTo2D reduceDimensionMinMultiPageTo2D = new CReduceDimensionMinMultiPageTo2D();
+
+				// Source 이미지 설정 // Set source image 
+				reduceDimensionMinMultiPageTo2D.SetSourceImage(ref fliSourceImage);
+
+				// Destination 이미지 설정 // Set destination image
+				reduceDimensionMinMultiPageTo2D.SetDestinationImage(ref fliDestinationImage);
+
+				// 알고리즘 수행 // Execute the algorithm
+				if((res = (reduceDimensionMinMultiPageTo2D.Execute())).IsFail())
+				{
+					ErrorPrint(res, "Failed to execute Reduce Dimension Min Multi Page To 2D. \n");
+					break;
+				}
+
+				// 출력을 위한 이미지 레이어를 얻어옵니다. //  Gets the image layer for output.
+				// 따로 해제할 필요 없음 // No need to release separately
+				CGUIViewImageLayer layerSource = viewImageSource.GetLayer(0);
+				CGUIViewImageLayer layerDestination = viewImageDestination.GetLayer(0);
+
+				// 기존에 Layer에 그려진 도형들을 삭제 // Delete the shapes drawn on the existing layer
+				layerSource.Clear();
+				layerDestination.Clear();
+
+				// View 정보를 디스플레이 합니다. // Display View information.
+				CFLPoint<double> flpPoint = new CFLPoint<double>(0, 0);
+
+				if((res = (layerSource.DrawTextCanvas(flpPoint, "Source Image", EColor.YELLOW, EColor.BLACK, 30))).IsFail())
+				{
+					ErrorPrint(res, "Failed to draw text. \n");
+					break;
+				}
+
+				if((res = layerDestination.DrawTextCanvas(flpPoint, "Destination Image(X Dimension)", EColor.YELLOW, EColor.BLACK, 30)).IsFail())
+				{
+					ErrorPrint(res, "Failed to draw text. \n");
+					break;
+				}
+
+				// 이미지 뷰를 갱신 합니다. // Update the image view.
+				viewImageSource.Invalidate(true);
+				viewImageDestination.Invalidate(true);
+
+				// 이미지와 이미지 뷰의 Sync 를 동일하게 맞춥니다.
+				if((res = (viewImageDestination.ZoomFit())).IsFail())
+				{
+					ErrorPrint(res, "Failed to zoom fit. \n");
+					break;
+				}
+
+				// 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
+				while(viewImageSource.IsAvailable() && viewImageDestination.IsAvailable())
+					Thread.Sleep(1);
+			}
+			while(false);
+		}
+	}
+}
