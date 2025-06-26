0f74487df4c0b983030a971c627c1833e15975dc
mjjeong <mjjeong@fourthlogic.co.kr>
2025-06-26 19:40:08 +0900

    Add Point Cloud Generator 3D Example

---
+++ PointCloudGenerator3D/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using FLImagingCLR;
+using FLImagingCLR.Base;
+using FLImagingCLR.Foundation;
+using FLImagingCLR.GUI;
+using FLImagingCLR.ImageProcessing;
+using FLImagingCLR.AdvancedFunctions;
+using FLImagingCLR.ThreeDim;
+using System.Diagnostics;
+using System.Collections;
+
+namespace PointCloudGenerator3D
+{
+	class Program
+	{
+        public static void ErrorPrint(CResult cResult, string str)
+        {
+            if (str.Length > 1)
+                Console.WriteLine(str);
+
+            Console.WriteLine("Error code : {0}\nError name : {1}\n", cResult.GetResultCode(), cResult.GetString());
+            Console.WriteLine("\n");
+            Console.ReadKey();
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // 이미지 뷰 선언 // Declare the image view
+            CGUIView3D view3DDst = new CGUIView3D();
+            CFL3DObject fl3DObjectDst = new CFL3DObject();
+
+            // 알고리즘 동작 결과 // Algorithm execution result
+            CResult res = new CResult();
+
+            do
+            {
+                if ((res = view3DDst.Create(600, 0, 1100, 500)).IsFail())
+                {
+                    ErrorPrint(res, "Failed to create the image view.\n");
+                    break;
+                }
+
+                CPointCloudGenerator3D alg = new CPointCloudGenerator3D();
+
+                // 파라미터 설정 // Set parameter
+                alg.SetDestinationObject(ref fl3DObjectDst);
+                alg.EnableColorGeneration(true);
+                alg.EnableNormalGeneration(false);
+
+                CPointCloudGenerator3D.SCountInfo sCount = new CPointCloudGenerator3D.SCountInfo();
+                sCount.bCopyOriginalVertices = false;
+                sCount.i64Edges = 0;
+                sCount.i64Faces = 0;
+                sCount.i64Interiors = 0;
+
+                sCount.bCopyOriginalVertices = true;
+                alg.AddPredefinedObject(sCount, EPredefinedObject.Regular_DodecaHedron, new TPoint3<Byte>(255, 255, 255));
+                sCount.bCopyOriginalVertices = false;
+
+                sCount.i64Edges = 4000;
+                alg.AddPredefinedObject(sCount, EPredefinedObject.Regular_DodecaHedron, new TPoint3<Byte>(255, 0, 0));
+                sCount.i64Edges = 0;
+
+                sCount.i64Faces = 20000;
+                alg.AddPredefinedObject(sCount, EPredefinedObject.Regular_DodecaHedron, new TPoint3<Byte>(0, 255, 0));
+                sCount.i64Faces = 0;
+
+                sCount.i64Interiors = 100000;
+                alg.AddPredefinedObject(sCount, EPredefinedObject.Regular_DodecaHedron, new TPoint3<Byte>(0, 0, 255));
+                sCount.i64Interiors = 0;
+
+
+                // 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+
+                if ((res = alg.Execute()).IsFail())
+                {
+                    ErrorPrint(res, "Failed to execute.");
+                    break;
+                }
+
+                // 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
+                // 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+                CGUIView3DLayer layer3DDst = view3DDst.GetLayer(0);
+
+                // 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+                layer3DDst.Clear();
+
+                // Destination 이미지가 새로 생성됨으로 Zoom fit 을 통해 디스플레이 되는 이미지 배율을 화면에 맞춰준다. // With the newly created Destination image, the image magnification displayed through Zoom fit is adjusted to the screen.
+                view3DDst.PushObject(fl3DObjectDst);
+                view3DDst.ZoomFit();
+
+                CFLPoint<double> flp = new CFLPoint<double>();
+
+                if ((res = layer3DDst.DrawTextCanvas(flp, "Destination Object", EColor.YELLOW, EColor.BLACK, 20)).IsFail())
+                {
+                    ErrorPrint(res, "Failed to draw text.\n");
+                    break;
+                }
+
+                // 이미지 뷰를 갱신 합니다. // Update image view
+                view3DDst.Invalidate(true);
+
+                // 이미지 뷰, 3D 뷰가 종료될 때 까지 기다림 // Wait for the image and 3D view to close
+                while (view3DDst.IsAvailable())
+                    Thread.Sleep(1);
+            }
+            while (false);
+        }
+	}
+}
