a4f1bcb08b1a37cb810956099d812bd64b7db571
jychoi <jychoi@fourthlogic.co.kr>
2025-07-02 15:29:35 +0900

    Renaming the example folder

--- BlobSubsampled_Min_Max/Properties/AssemblyInfo.cs
+++ BlobSubsampled_Min_Max/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BlobSubsampled_MinMax")]
+[assembly: AssemblyTitle("BlobSubsampled_Min_Max")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BlobSubsampled_MinMax")]
+[assembly: AssemblyProduct("BlobSubsampled_Min_Max")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
