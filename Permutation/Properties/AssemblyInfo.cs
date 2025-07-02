446842bf2ba155c36df554f8ac5c23d0814ba78e
jblee <jblee@fourthlogic.co.kr>
2025-07-02 15:00:18 +0900

    Fix types in the Permutation example folder

--- Permutation/Properties/AssemblyInfo.cs
+++ Permutation/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Premutation")]
+[assembly: AssemblyTitle("Permutation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Premutation")]
+[assembly: AssemblyProduct("Permutation")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
