89f44b3db447ea3b262e1f7eeb6843d72b2c0839
jhsong <jhsong@fourthlogic.co.kr>
2025-07-26 20:36:48 +0900

    ImageViewIntoWPF: Fix build errors

--- ImageViewIntoWPF/Properties/Resources.Designer.cs
+++ ImageViewIntoWPF/Properties/Resources.Designer.cs
@@ -44,7 +44,7 @@ namespace FLImagingExamplesCSharp
 			{
                 if ((resourceMan == null))
 				{
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ImageViewIntoWPF.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FLImagingExamplesCSharp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
