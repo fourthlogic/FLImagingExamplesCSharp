edf1c95799d954a4b2f9c9a69114c6de43e8480c
sypark <sypark@fourthlogic.co.kr>
2025-07-26 19:49:24 +0900

    Improved comments for DrawTextCanvas()

--- View3D/View3D.cs
+++ View3D/View3D.cs
@@ -156,13 +156,11 @@ namespace View3D
 				view3D.GetLayer(0).Clear();
 
 				// View 정보를 디스플레이 합니다. // Display the view information.
-				// 아래 함수 DrawTextCanvas() 는 Screen 좌표를 기준으로 하는 문자열을 Drawing 한다. // The function DrawTextCanvas() draws a String based on the screen coordinates.
-				// 파라미터 순서 : 레이어 -> 기준 좌표 Figure 객체 -> 문자열 -> 폰트 색 -> 면 색 -> 폰트 크기 -> 실제 크기 유무 -> 각도 ->
-				//                 얼라인 -> 폰트 이름 -> 폰트 알파값(불투명도) -> 면 알파값 (불투명도) -> 폰트 두께 -> 폰트 이텔릭
-				// Parameter order: layer -> reference coordinate Figure object -> string -> font color -> Area color -> font size -> actual size -> angle ->
-				//                  Align -> Font Name -> Font Alpha Value (Opaqueness) -> Cotton Alpha Value (Opaqueness) -> Font Thickness -> Font Italic
+				// 아래 함수 DrawTextCanvas는 스크린 좌표를 기준으로 문자열을 뷰어에 출력한다.
+				// The function DrawTextCanvas displays a string on the viewer using screen coordinates.
+				// 파라미터 순서 : 기준 좌표 Figure 객체 -> 문자열 -> 텍스트 색 -> 텍스트 테두리 색 -> 폰트 크기 -> 실제 크기로 출력 유무 -> 각도 -> 정렬 -> 폰트 이름 -> 텍스트 알파값(불투명도) -> 텍스트 테두리 알파값 (불투명도) -> 폰트 두께 -> 폰트 이탤릭 여부
+				// Parameter order: reference coordinate (Figure object) -> text string -> text color -> text outline color -> font size -> render in real-world size (bool) -> angle -> alignment -> font name -> text alpha (opacity) -> text outline alpha (opacity) -> font thickness -> italic font (bool)
 				CFLPoint<double> flpPosition = new CFLPoint<double>(0, 0);
-
 				if((res = (arrLayer[(int)EType.Model].DrawTextCanvas(flpPosition, "Model Image", EColor.YELLOW, EColor.BLACK, 30))).IsFail())
 				{
 					ErrorPrint(res, "Failed to draw text.\n");
