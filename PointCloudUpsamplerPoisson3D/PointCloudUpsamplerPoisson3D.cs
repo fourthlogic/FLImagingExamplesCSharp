aab4c58d31e776a9d26814011af26da65f65a6e5
hskim <hskim@fourthlogic.co.kr>
2025-07-28 21:47:20 +0900

    Rename example file names, Modify class name, Rename class instance for consistency

--- PointCloudUpsamplerPoisson3D/PointCloudUpsamplerPoisson3D.cs
+++ PointCloudUpsamplerPoisson3D/PointCloudUpsamplerPoisson3D.cs
@@ -16,40 +16,40 @@ using System.Collections;
 
 namespace FLImagingExamplesCSharp
 {
-	class UniformPointCloudUpsampler3D
+	class PointCloudUpsamplerPoisson3D
 	{
-		public static void ErrorPrint(CResult cResult, string str)
+        public static void ErrorPrint(CResult cResult, string str)
 		{
-			if(str.Length > 1)
-				Console.WriteLine(str);
+            if (str.Length > 1)
+                Console.WriteLine(str);
 
-			Console.WriteLine("Error code : {0}\nError name : {1}\n", cResult.GetResultCode(), cResult.GetString());
-			Console.WriteLine("\n");
-			Console.ReadKey();
-		}
+            Console.WriteLine("Error code : {0}\nError name : {1}\n", cResult.GetResultCode(), cResult.GetString());
+            Console.WriteLine("\n");
+            Console.ReadKey();
+        }
 
-		[STAThread]
-		static void Main(string[] args)
+        [STAThread]
+        static void Main(string[] args)
 		{
 			// You must call the following function once
 			// before using any features of the FLImaging(R) library
 			CLibraryUtilities.Initialize();
 
 			// 이미지 뷰 선언 // Declare the image view
-			CGUIView3D view3DSrc = new CGUIView3D();
+			CGUIView3D view3DSrc= new CGUIView3D();
 			CGUIView3D view3DDst = new CGUIView3D();
 
 			// 알고리즘 동작 결과 // Algorithm execution result
 			CResult res = new CResult();
 
-			do
+            do
 			{
-				if((res = view3DSrc.Create(100, 0, 600, 500)).IsFail() ||
+                if ((res = view3DSrc.Create(100, 0, 600, 500)).IsFail() ||
 					(res = view3DDst.Create(600, 0, 1100, 500)).IsFail())
 				{
-					ErrorPrint(res, "Failed to create the image view.\n");
-					break;
-				}
+                    ErrorPrint(res, "Failed to create the image view.\n");
+                    break;
+                }
 
 				view3DSrc.PushObject(new CFL3DObject());
 				var viewObjectSrc = view3DSrc.GetView3DObject(0);
@@ -65,17 +65,17 @@ namespace FLImagingExamplesCSharp
 					break;
 				}
 
-				CUniformPointCloudUpsampler3D alg = new CUniformPointCloudUpsampler3D();
+				CPointCloudUpsamplerPoisson3D pointCloudUpsamplerPoisson3D = new CPointCloudUpsamplerPoisson3D();
 
 				// 파라미터 설정 // Set parameter
-				alg.SetSourceObject(ref floSrc);
-				alg.SetDestinationObject(ref floDst);
-				alg.SetColoringMode(CPointCloudUpsampler3DBase.EColoringMode.Interpolate);
-				alg.EnableNormalInterpolation(true);
-				alg.SetSamplingSize((long)1e7);
-				alg.EnableCopyVertex(true);
-				alg.EnableFaceReconstruction(false);
-				alg.EnableFaceRetainment(false);
+				pointCloudUpsamplerPoisson3D.SetSourceObject(ref floSrc);
+				pointCloudUpsamplerPoisson3D.SetDestinationObject(ref floDst);
+				pointCloudUpsamplerPoisson3D.SetColoringMode(CPointCloudUpsampler3DBase.EColoringMode.Interpolate);
+				pointCloudUpsamplerPoisson3D.EnableNormalInterpolation(true);
+				pointCloudUpsamplerPoisson3D.EnableAutoDistance(true);
+				pointCloudUpsamplerPoisson3D.EnableCopyVertex(true);
+				pointCloudUpsamplerPoisson3D.EnableFaceReconstruction(false);
+				pointCloudUpsamplerPoisson3D.EnableFaceRetainment(false);
 				// 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
 				// 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
 				CGUIView3DLayer layer3DSrc = view3DSrc.GetLayer(0);
@@ -85,17 +85,18 @@ namespace FLImagingExamplesCSharp
 				layer3DSrc.Clear();
 				layer3DDst.Clear();
 
-				CFLPoint<double> flpTopLeft = new CFLPoint<double>(0, 0);
 
-				if((res = layer3DSrc.DrawTextCanvas(flpTopLeft, "Source Object", EColor.YELLOW, EColor.BLACK, 20)).IsFail() ||
+				CFLPoint<double> flpTopLeft = new CFLPoint<double>(0, 0);
+				
+				if((res = layer3DSrc.DrawTextCanvas(flpTopLeft, "Source Object", EColor.YELLOW, EColor.BLACK, 20)).IsFail() || 
 					(res = layer3DDst.DrawTextCanvas(flpTopLeft, "Destination Object", EColor.YELLOW, EColor.BLACK, 20)).IsFail())
 				{
 					ErrorPrint(res, "Failed to draw text.\n");
 					break;
 				}
 
-				// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
-				if((res = alg.Execute()).IsFail())
+				// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute pointCloudUpsamplerPoisson3Dorithm according to previously set parameters
+				if((res = pointCloudUpsamplerPoisson3D.Execute()).IsFail())
 				{
 					ErrorPrint(res, "Failed to execute.");
 					break;
@@ -126,8 +127,8 @@ namespace FLImagingExamplesCSharp
 				// 이미지 뷰, 3D 뷰가 종료될 때 까지 기다림 // Wait for the image and 3D view to close
 				while(view3DDst.IsAvailable())
 					Thread.Sleep(1);
-			}
-			while(false);
-		}
+            }
+            while (false);
+        }
 	}
 }
