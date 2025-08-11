f4197d7198ac0ea1768b926138b94aab836f0912
jblee <jblee@fourthlogic.co.kr>
2025-08-11 14:33:24 +0900

    Modify parameters in the Alignment Tiling example

--- AlignmentTiling/AlignmentTiling.cs
+++ AlignmentTiling/AlignmentTiling.cs
@@ -154,9 +154,14 @@ namespace FLImagingExamplesCSharp
 				// 결과 이미지의 크기 지정유무 옵션 설정 // Set whether or not to specify the size of the resulting image
 				alignmentTiling.SetResizeMethod(EResizeMethod.Resize);
 				// Resize일 경우, 결과 이미지의 크기 설정 // In case of Resize, set the size of the resulting image
-				alignmentTiling.SetResultImageSize(780, 780);
+				alignmentTiling.SetResultImageSize(1500, 1500);
 				// 보정에 사용할 Accuracy 설정 // Set the Accuracy to be used for calibration
 				alignmentTiling.SetAccuracy(EFloatingPointAccuracy.Bit32);
+				// 결과 이미지의 중심이 될 위치를 설정 // Set the location to be the center of the resulting image
+				CFLPoint<double> flpPivotPoint = new CFLPoint<double>(192, 778);
+				alignmentTiling.SetSourcePivot(flpPivotPoint);
+				// Pivot이 지정된 Page Index를 설정 // Set the Page Index with Pivot specified
+				alignmentTiling.SetPivotPageIndex(1);
 
 				// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
 				if((res = (alignmentTiling.Execute())).IsFail())
