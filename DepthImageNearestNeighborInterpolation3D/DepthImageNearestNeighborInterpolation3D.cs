a283c37866306482e0fab3008580ab76daa420af
swbang <swbang@fourthlogic.co.kr>
2025-09-04 20:13:34 +0900

    Add Depth Image Nearest Neighbor Interpolation 3D Example

---
+++ DepthImageNearestNeighborInterpolation3D/DepthImageNearestNeighborInterpolation3D.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using FLImagingCLR;
+using FLImagingCLR.Base;
+using FLImagingCLR.Foundation;
+using FLImagingCLR.GUI;
+using FLImagingCLR.ImageProcessing;
+using FLImagingCLR.AdvancedFunctions;
+using FLImagingCLR.ThreeDim;
+using CResult = FLImagingCLR.CResult;
+
+namespace FLImagingExamplesCSharp
+{
+	class DepthImageNearestNeighborInterpolation3D
+	{
+		public static void ErrorPrint(CResult cResult, string str)
+		{
+			if(str.Length > 1)
+				Console.WriteLine(str);
+
+			Console.WriteLine("Error code : {0}\nError name : {1}\n", cResult.GetResultCode(), cResult.GetString());
+			Console.WriteLine("\n");
+			Console.ReadKey();
+		}
+
+		[STAThread]
+		static void Main(string[] args)
+		{
+			// You must call the following function once
+			// before using any features of the FLImaging(R) library
+			CLibraryUtilities.Initialize();
+
+			// 이미지 객체 선언 // Declare the image object
+			CFLImage fliSourceImage = new CFLImage();
+			CFLImage fliDestinationImage = new CFLImage();
+
+			// 이미지 뷰 선언 // Declare the image view
+			CGUIViewImage viewImageSource = new CGUIViewImage();
+			CGUIViewImage viewImageDestination = new CGUIViewImage();
+
+			do
+			{
+				CResult res;
+				// 이미지 로드 // Load image
+				if((res = fliSourceImage.Load("../../ExampleImages/DepthImageNearestNeighborInterpolation3D/Source.flif")).IsFail())
+				{
+					ErrorPrint(res, "Failed to load the image file.\n");
+					break;
+				}
+
+				// 이미지 뷰 생성 // Create image view
+				if((res = viewImageSource.Create(100, 0, 612, 512)).IsFail())
+				{
+					ErrorPrint(res, "Failed to create the image view.\n");
+					break;
+				}
+
+				// 이미지 뷰 생성 // Create image view
+				if((res = viewImageDestination.Create(612, 0, 1124, 512)).IsFail())
+				{
+					ErrorPrint(res, "Failed to create the image view.\n");
+					break;
+				}
+
+				// 두 이미지 뷰 윈도우의 위치를 동기화 한다 // Synchronize the positions of the two image view windows
+				if((res = viewImageSource.SynchronizeWindow(ref viewImageDestination)).IsFail())
+				{
+					ErrorPrint(res, "Failed to synchronize window. \n");
+					break;
+				}
+
+				// 두 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the two image views. .
+				if((res = viewImageSource.SynchronizePointOfView(ref viewImageDestination)).IsFail())
+				{
+					ErrorPrint(res, "Failed to synchronize view. \n");
+					break;
+				}
+
+				// 이미지 뷰에 이미지를 디스플레이 // Display the image in the image view
+				if((res = viewImageSource.SetImagePtr(ref fliSourceImage)).IsFail())
+				{
+					ErrorPrint(res, "Failed to set image object on the image view. \n");
+					break;
+				}
+
+				// 이미지 뷰에 이미지를 디스플레이 // Display the image in the image view
+				if((res = viewImageDestination.SetImagePtr(ref fliDestinationImage)).IsFail())
+				{
+					ErrorPrint(res, "Failed to set image object on the image view. \n");
+					break;
+				}
+
+				// Depth Image Nearest Neighbor Interpolation 3D 객체 생성 // Create Depth Image Nearest Neighbor Interpolation 3D object
+				CDepthImageNearestNeighborInterpolation3D depthImageNearestNeighborInterpolation3D = new CDepthImageNearestNeighborInterpolation3D();
+
+				// Source 이미지 설정 // Set source image 
+				depthImageNearestNeighborInterpolation3D.SetSourceImage(ref fliSourceImage);
+
+				// Destination 이미지 설정 // Set destination image
+				depthImageNearestNeighborInterpolation3D.SetDestinationImage(ref fliDestinationImage);
+
+				// Page Position 설정 // Set page position
+				List<double> flaPagePosition = new List<double>();
+				flaPagePosition.Add(0);
+				flaPagePosition.Add(1);
+				flaPagePosition.Add(2);
+			
+				depthImageNearestNeighborInterpolation3D.SetPagePosition(flaPagePosition);
+
+				// Interval 설정 // Set interval
+				depthImageNearestNeighborInterpolation3D.SetInterval(0.1);
+
+				// Start Position 설정 // Set start position
+				depthImageNearestNeighborInterpolation3D.SetStartPosition(0);
+
+				// End Position 설정 // Set end position
+
+				depthImageNearestNeighborInterpolation3D.SetEndPosition(2);
+				// 알고리즘 수행 // Execute the algorithm
+				if((res = (depthImageNearestNeighborInterpolation3D.Execute())).IsFail())
+				{
+					ErrorPrint(res, "Failed to execute Depth Image Nearest Neighbor Interpolation 3D. \n");
+					break;
+				}
+
+				// 출력을 위한 이미지 레이어를 얻어옵니다. //  Gets the image layer for output.
+				// 따로 해제할 필요 없음 // No need to release separately
+				CGUIViewImageLayer layerSource = viewImageSource.GetLayer(0);
+				CGUIViewImageLayer layerDestination = viewImageDestination.GetLayer(0);
+
+				// 기존에 Layer에 그려진 도형들을 삭제 // Delete the shapes drawn on the existing layer
+				layerSource.Clear();
+				layerDestination.Clear();
+
+				// View 정보를 디스플레이 합니다. // Display View information.
+				CFLPoint<double> flpPoint = new CFLPoint<double>(0, 0);
+
+				if((res = (layerSource.DrawTextCanvas(flpPoint, "Source Image", EColor.YELLOW, EColor.BLACK, 30))).IsFail())
+				{
+					ErrorPrint(res, "Failed to draw text. \n");
+					break;
+				}
+
+				if((res = layerDestination.DrawTextCanvas(flpPoint, "Destination Image", EColor.YELLOW, EColor.BLACK, 30)).IsFail())
+				{
+					ErrorPrint(res, "Failed to draw text. \n");
+					break;
+				}
+
+				// 이미지 뷰를 갱신 합니다. // Update the image view.
+				viewImageSource.Invalidate(true);
+				viewImageDestination.Invalidate(true);
+
+				// 이미지와 이미지 뷰의 Sync 를 동일하게 맞춥니다.
+				if((res = (viewImageDestination.ZoomFit())).IsFail())
+				{
+					ErrorPrint(res, "Failed to zoom fit. \n");
+					break;
+				}
+
+				// 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
+				while(viewImageSource.IsAvailable() && viewImageDestination.IsAvailable())
+					Thread.Sleep(1);
+			}
+			while(false);
+		}
+	}
+}
