46997ff6e8473561efaac213b2b04761607bd369
thkim <kth@fourthlogic.co.kr>
2025-12-26 11:07:41 +0900

    Modify enum name in OrientedObjectDetection Example

--- OrientedObjectDetctionDL/OrientedObjectDetction.cs
+++ OrientedObjectDetctionDL/OrientedObjectDetction.cs
@@ -200,9 +200,9 @@ namespace FLImagingExamplesCSharp
 				orientedObjectDetectionDL.SetInferenceResultImage(ref fliResultImage);
 
 				// 학습할 OrientedObjectDetection 모델 설정 // Set up the OrientedObjectDetection model to learn
-				orientedObjectDetectionDL.SetModel(COrientedObjectDetectionDL.EModel.Oriented_R_FLNet);
+				orientedObjectDetectionDL.SetModel(COrientedObjectDetectionDL.EModel.OR_FLNet);
 				// 학습할 OrientedObjectDetection 모델 Version 설정 // Set up the OrientedObjectDetection model version to learn
-				orientedObjectDetectionDL.SetModelVersion(COrientedObjectDetectionDL.EModelVersion.Oriented_R_FLNet_V1_256);
+				orientedObjectDetectionDL.SetModelVersion(COrientedObjectDetectionDL.EModelVersion.OR_FLNet_V1_256);
 				// 학습 epoch 값을 설정 // Set the learn epoch value 
 				orientedObjectDetectionDL.SetLearningEpoch(1024);
 				// 학습 이미지 Interpolation 방식 설정 // Set Interpolation method of learn image
