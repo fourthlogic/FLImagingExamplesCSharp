f4c3db70f15d8dddadc25172eb5afa5ed4a5f921
jjpark <jjpark@fourthlogic.co.kr>
2026-01-13 10:46:15 +0900

    Apply ROI in BM3D Filter Example

--- BM3DFilter/BM3DFilter.cs
+++ BM3DFilter/BM3DFilter.cs
@@ -144,11 +144,17 @@ namespace FLImagingExamplesCSharp
 					break;
 				}
 
+				// ROI 설정을 위한 CFLRect 객체 생성 // Create a CFLRect object for setting ROI
+				CFLRect<int> flrROI = new CFLRect<int>(200, 200, 500, 500);
+
 				// BM3DFilter 객체 생성 // Create BM3DFilter object
 				CBM3DFilter bm3dFilter = new CBM3DFilter();
 
 				// 처리할 이미지 설정 // Set the image to process
 				bm3dFilter.SetSourceImage(ref fliSrcImage);
+
+				// Source ROI 설정 // Set the Source ROI
+				bm3dFilter.SetSourceROI(flrROI);
 
 				// Destination 이미지 설정 // Set the destination image
 				bm3dFilter.SetDestinationImage(ref fliDst1Image);
@@ -185,6 +191,32 @@ namespace FLImagingExamplesCSharp
 				CGUIViewImageLayer layerDst1 = viewImageDst1.GetLayer(0);
 				CGUIViewImageLayer layerDst2 = viewImageDst2.GetLayer(0);
 
+				// 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+				layerSrc.Clear();
+				layerDst1.Clear();
+				layerDst2.Clear();
+
+				// ROI영역이 어디인지 알기 위해 디스플레이 한다 // Display to find out where ROI is
+				// FLImaging의 Figure 객체들은 어떤 도형모양이든 상관없이 하나의 함수로 디스플레이가 가능 // FLimaging's Figure objects can be displayed as a function regardless of the shape
+				// 아래 함수 DrawFigureImage는 Image좌표를 기준으로 하는 Figure를 Drawing 한다는 것을 의미하며 // The function DrawFigureImage below means drawing a picture based on the image coordinates
+				// 맨 마지막 두개의 파라미터는 불투명도 값이고 1일 경우 불투명, 0일 경우 완전 투명을 의미한다. // The last two parameters are opacity values, which mean opacity for 1 day and complete transparency for 0 day.
+				// 파라미터 순서 : 레이어 -> Figure 객체 -> 선 색 -> 선 두께 -> 면 색 -> 펜 스타일 -> 선 알파값(불투명도) -> 면 알파값 (불투명도) // Parameter order: Layer -> Figure object -> Line color -> Line thickness -> Face color -> Pen style -> Line alpha value (opacity) -> Area alpha value (opacity)
+				if((res = layerSrc.DrawFigureImage(flrROI, EColor.LIME)).IsFail())
+					ErrorPrint(res, "Failed to draw figure.\n");
+
+				if((res = layerDst1.DrawFigureImage(flrROI, EColor.LIME)).IsFail())
+					ErrorPrint(res, "Failed to draw figure.\n");
+
+				if((res = layerDst2.DrawFigureImage(flrROI, EColor.LIME)).IsFail())
+					ErrorPrint(res, "Failed to draw figure.\n");
+
+				// View 정보를 디스플레이 한다. // Display view information
+				// 아래 함수 DrawTextCanvas은 Screen 좌표를 기준으로 하는 String을 Drawing 한다. // The function DrawTextCanvas below draws a String based on the screen coordinates.
+				// 색상 파라미터를 EGUIViewImageLayerTransparencyColor 으로 넣어주게되면 배경색으로 처리함으로 불투명도를 0으로 한것과 같은 효과가 있다. // If the color parameter is added as EGUIViewImageLayerTransparencyColor, it has the same effect as setting the opacity to 0 by processing it as a background color.
+				// 파라미터 순서 : 레이어 -> 기준 좌표 Figure 객체 -> 문자열 -> 폰트 색 -> 면 색 -> 폰트 크기 -> 실제 크기 유무 -> 각도 ->
+				//                 얼라인 -> 폰트 이름 -> 폰트 알파값(불투명도) -> 면 알파값 (불투명도) -> 폰트 두께 -> 폰트 이텔릭
+				// Parameter order: layer -> reference coordinate Figure object -> string -> font color -> Area color -> font size -> actual size -> angle ->
+				//                  Align -> Font Name -> Font Alpha Value (Opaqueness) -> Cotton Alpha Value (Opaqueness) -> Font Thickness -> Font Italic
 				CFLPoint<double> flp = new CFLPoint<double>();
 
 				if((res = layerSrc.DrawTextCanvas(flp, ("Source Image"), EColor.YELLOW, EColor.BLACK, 20)).IsFail())
