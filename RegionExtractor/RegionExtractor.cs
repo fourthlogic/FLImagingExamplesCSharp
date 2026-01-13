30845bedc37fc6d526e43483c2951c915b5db7ee
jychoi <jychoi@fourthlogic.co.kr>
2026-01-13 09:16:26 +0900

    Update Region Extractor Example (Image Figures as Extraction Regions API Update)

--- RegionExtractor/RegionExtractor.cs
+++ RegionExtractor/RegionExtractor.cs
@@ -115,14 +115,12 @@ namespace FLImagingExamplesCSharp
 				CMultiVar<double> mv = new CMultiVar<double>(0);
 				regionExtractor.SetBlankColor(mv);
 
-				// 추가 추출 영역 생성 // Add an additional extraction area
-				CFLFigureArray flfaExtractRegion = new CFLFigureArray();
-
-				flfaExtractRegion.PushBack(new CFLCircle<double>(314, 197, 60));
-				flfaExtractRegion.PushBack(new CFLCircle<double>(103, 198, 60));
-
-				// 추출 영역을 추가로 API로 지정 // Additional extraction areas can be specified via the API
-				regionExtractor.SetExtractionRegion(flfaExtractRegion);
+				// 이미지 Figure 를 추출 영역으로 설정 // Enable image figures as extraction regions
+				regionExtractor.EnableImageFiguresAsExtractionRegions(true);
+				// 이미지 Figure 를 연산 결과에 반영 // Reflect the image figure in the computation results
+				regionExtractor.EnableImageFigureTransformation(true);
+				// 이미지 Figure연산 결과 중 이미지를 벗어나는 figure를 제거 // Remove figures that extend beyond the image among the image figure computation results.
+				regionExtractor.EnableImageFigureTransformationRetainOutside(false);
 
 				// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
 				if((res = regionExtractor.Execute()).IsFail())
