6473558bfbabffdd210dca31e5782133f66641c3
jslee <jslee@fourthlogic.co.kr>
2026-02-13 14:40:02 +0900

    Add FL Depth Image Reconstruction 3D example

---
+++ FLDepthImageReconstruction3D/FLDepthImageReconstruction3D.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using FLImagingCLR;
+using FLImagingCLR.Base;
+using FLImagingCLR.Foundation;
+using FLImagingCLR.GUI;
+using FLImagingCLR.ImageProcessing;
+using FLImagingCLR.AdvancedFunctions;
+using FLImagingCLR.ThreeDim;
+
+namespace FLImagingExamplesCSharp
+{
+	class FLDepthImageReconstruction3D
+	{
+		public static void ErrorPrint(CResult cResult, string str)
+		{
+			if(str.Length > 1)
+				Console.WriteLine(str);
+
+			Console.WriteLine("Error code : {0}\nError name : {1}\n", cResult.GetResultCode(), cResult.GetString());
+			Console.WriteLine("\n");
+			Console.ReadKey();
+		}
+
+		[STAThread]
+		static void Main(string[] args)
+		{
+			// You must call the following function once
+			// before using any features of the FLImaging(R) library
+			CLibraryUtilities.Initialize();
+
+			// 이미지 객체 선언 // Declare the image object
+			CFLImage fliSrcImage = new CFLImage();
+			CFLImage fliDstImage = new CFLImage();
+			CFLImage fliTxtImage = new CFLImage();
+			CFL3DObject floDstObject = new CFL3DObjectHeightMap();
+
+			// 이미지 뷰 선언 // Declare the image view
+			CGUIViewImage viewImageSrc = new CGUIViewImage();
+			CGUIViewImage viewImageDst = new CGUIViewImage();
+			CGUIView3D view3DDst = new CGUIView3D();
+
+			// 알고리즘 동작 결과 // Algorithm execution result
+			CResult res;
+
+			do
+			{
+				// 이미지 로드 // Load image
+				if((res = fliSrcImage.Load("../../ExampleImages/DepthImageReconstruction3D/")).IsFail())
+				{
+					ErrorPrint(res, "Failed to load the image file.\n");
+					break;
+				}
+
+				// 이미지 뷰 생성 // Create image view
+				if((res = viewImageSrc.Create(100, 0, 600, 500)).IsFail() ||
+					(res = viewImageDst.Create(600, 0, 1100, 500)).IsFail() ||
+					(res = view3DDst.Create(400, 200, 1300, 800)).IsFail())
+				{
+					ErrorPrint(res, "Failed to create the image view.\n");
+					break;
+				}
+
+				// 두 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the two image views
+				if((res = viewImageSrc.SynchronizePointOfView(ref viewImageDst)).IsFail())
+				{
+					ErrorPrint(res, "Failed to synchronize view. \n");
+					break;
+				}
+
+				// 두 뷰 윈도우의 위치를 동기화 한다 // Synchronize the position of the two view windows.
+				if((res = viewImageSrc.SynchronizeWindow(ref viewImageDst)).IsFail())
+				{
+					ErrorPrint(res, "Failed to synchronize view. \n");
+					break;
+				}
+
+				// 이미지 뷰에 이미지를 디스플레이 // Display an image in an image view
+				if((res = viewImageSrc.SetImagePtr(ref fliSrcImage)).IsFail() ||
+					(res = viewImageDst.SetImagePtr(ref fliDstImage)).IsFail())
+				{
+					ErrorPrint(res, "Failed to set image object on the image view.\n");
+					break;
+				}
+
+				viewImageSrc.SetFixThumbnailView(true);
+
+
+				// 알고리즘 객체 생성 // Create algorithm object
+				CFLDepthImageReconstruction3D flDepthImageReconstruction3D = new CFLDepthImageReconstruction3D();
+
+				if((res = flDepthImageReconstruction3D.SetSourceImage(ref fliSrcImage)).IsFail())
+					break;
+				if((res = flDepthImageReconstruction3D.SetDestinationHeightMapImage(ref fliDstImage)).IsFail())
+					break;
+				if((res = flDepthImageReconstruction3D.SetDestinationTextureImage(ref fliTxtImage)).IsFail())
+					break;
+				if((res = flDepthImageReconstruction3D.SetDestinationObject(ref floDstObject)).IsFail())
+					break;
+
+				if((res = flDepthImageReconstruction3D.SetBackgroundPageIndex(1.00)).IsFail())
+					break;
+				if((res = flDepthImageReconstruction3D.SetDifferentialMethod(CFLDepthImageReconstruction3D.EDifferentialMethod.DoGY)).IsFail())
+					break;
+				if((res = flDepthImageReconstruction3D.SetSigma1(0.40)).IsFail())
+					break;
+				if((res = flDepthImageReconstruction3D.SetSigma2(0.60)).IsFail())
+					break;
+				if((res = flDepthImageReconstruction3D.SetKernelBias(0.001)).IsFail())
+					break;
+				if((res = flDepthImageReconstruction3D.SetFMThreshold(0.000002)).IsFail())
+					break;
+				if((res = flDepthImageReconstruction3D.SetTransitionGain(100.00)).IsFail())
+					break;
+				if((res = flDepthImageReconstruction3D.SetSigmaSpatial(11.00)).IsFail())
+					break;
+				if((res = flDepthImageReconstruction3D.SetSigmaRange(0.05)).IsFail())
+					break;
+				if((res = flDepthImageReconstruction3D.SetDirection(CFLDepthImageReconstruction3D.EDirection.BottomToTop)).IsFail())
+					break;
+				if((res = flDepthImageReconstruction3D.SetPixelAccuracy(0.10)).IsFail())
+					break;
+				if((res = flDepthImageReconstruction3D.SetDepthPitch(0.80)).IsFail())
+					break;
+
+				// 알고리즘 수행 // Execute the algorithm
+				if((res = flDepthImageReconstruction3D.Execute()).IsFail())
+				{
+					ErrorPrint(res, "Failed to execute the algorithm.");
+					break;
+				}
+
+
+				// 3D 이미지 뷰에 Height Map (Destination Image) 이미지를 디스플레이 // Display the Height Map (Destination Image) on the 3D image view
+				if(view3DDst.PushObject(floDstObject).IsFail())
+				{
+					ErrorPrint(res, "Failed to set image object on the image view.\n");
+					break;
+				}
+
+				// 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
+				// 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
+				CGUIViewImageLayer layerSrc = viewImageSrc.GetLayer(0);
+				CGUIViewImageLayer layerDst = viewImageDst.GetLayer(0);
+				CGUIView3DLayer layer3D = view3DDst.GetLayer(0);
+
+				// 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
+				layerSrc.Clear();
+				layerDst.Clear();
+				layer3D.Clear();
+
+				// 이미지 뷰 정보 표시 // Display image view information
+				CFLPoint<double> flp = new CFLPoint<double>();
+				if((res = layerSrc.DrawTextCanvas(flp, ("Source Image"), EColor.YELLOW, EColor.BLACK, 20)).IsFail() ||
+					(res = layerDst.DrawTextCanvas(flp, ("Destination Image"), EColor.YELLOW, EColor.BLACK, 20)).IsFail() ||
+					(res = layer3D.DrawTextCanvas(flp, "Destination Image", EColor.YELLOW, EColor.BLACK, 20)).IsFail())
+				{
+					ErrorPrint(res, "Failed to draw text.\n");
+					break;
+				}
+
+				viewImageSrc.SetLayerAutoClearMode(ELayerAutoClearMode.PageChanged, false);
+
+				// Zoom Fit
+				viewImageSrc.ZoomFit();
+				viewImageDst.ZoomFit();
+				view3DDst.ZoomFit();
+
+				// 이미지 뷰를 갱신 합니다. // Update image view
+				viewImageSrc.Invalidate(true);
+				viewImageDst.Invalidate(true);
+				view3DDst.Invalidate(true);
+
+				// 이미지 뷰, 3D 뷰가 종료될 때 까지 기다림 // Wait for the image and 3D view to close
+				while(viewImageSrc.IsAvailable() && viewImageDst.IsAvailable() && view3DDst.IsAvailable())
+					Thread.Sleep(1);
+			}
+			while(false);
+		}
+	}
+}
