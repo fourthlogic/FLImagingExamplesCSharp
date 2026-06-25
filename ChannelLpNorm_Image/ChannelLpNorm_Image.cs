6d09ea4e2ed6e07eb4c44013773a36678f668019
shhwang <shhwang@fourthlogic.co.kr>
2026-06-25 15:47:25 +0900

    Add Channel Lp Norm Example

---
+++ ChannelLpNorm_Image/ChannelLpNorm_Image.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using FLImagingCLR;
+using FLImagingCLR.Base;
+using FLImagingCLR.Foundation;
+using FLImagingCLR.GUI;
+using FLImagingCLR.ImageProcessing;
+using FLImagingCLR.AdvancedFunctions;
+using CResult = FLImagingCLR.CResult;
+
+namespace FLImagingExamplesCSharp
+{
+	class ChannelLpNorm
+	{
+		public static void ErrorPrint(CResult cResult, string str)
+		{
+			if(str.Length > 1)
+				Console.WriteLine(str);
+
+			Console.WriteLine("Error code : {0}\nError name : {1}\n", cResult.GetResultCode(), cResult.GetString());
+			Console.WriteLine("\n");
+			Console.ReadKey();
+		}
+
+		[STAThread]
+		static void Main(string[] args)
+		{
+			// You must call the following function once
+			// before using any features of the FLImaging(R) library
+			CLibraryUtilities.Initialize();
+
+			// 이미지 객체 선언 // Declare the image object
+			CFLImage fliSrcImage = new CFLImage();
+			CFLImage fliOprImage = new CFLImage();
+			CFLImage fliDstImage = new CFLImage();
+
+			// 이미지 뷰 선언 // Declare the image view
+			CGUIViewImage viewImageSrc = new CGUIViewImage();
+			CGUIViewImage viewImageOpr = new CGUIViewImage();
+			CGUIViewImage viewImageDst = new CGUIViewImage();
+
+			do
+			{
+				CResult res;
+				// 이미지 로드 // Load image
+				if((res = fliSrcImage.Load("../../ExampleImages/ChannelLpNorm/Coord.flif")).IsFail())
+				{
+					//ErrorPrint(res, "Failed to load the image file.\n");
+					break;
+				}
+
+				if((res = fliOprImage.Load("../../ExampleImages/ChannelLpNorm/Gradation.flif")).IsFail())
+				{
+					//ErrorPrint(res, "Failed to load the image file.\n");
+					break;
+				}
+
+				// 이미지 뷰 생성 // Create image view
+				if((res = viewImageSrc.Create(100, 0, 600, 500)).IsFail() ||
+					(res = viewImageOpr.Create(600, 0, 1100, 500)).IsFail() ||
+					(res = viewImageDst.Create(1100, 0, 1600, 500)).IsFail())
+				{
+					//ErrorPrint(res, "Failed to create the image view.\n");
+					break;
+				}
+
+				// 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the image views. .
+				if((res = viewImageSrc.SynchronizePointOfView(ref viewImageOpr)).IsFail())
+				{
+					//ErrorPrint(res, "Failed to synchronize view. \n");
+					break;
+				}
+
+				if((res = viewImageSrc.SynchronizePointOfView(ref viewImageDst)).IsFail())
+				{
+					//ErrorPrint(res, "Failed to synchronize view. \n");
+					break;
+				}
+
+				// 이미지 뷰 윈도우의 위치를 동기화 한다 // Synchronize the positions of the image view windows
+				if((res = viewImageSrc.SynchronizeWindow(ref viewImageOpr)).IsFail())
+				{
+					//ErrorPrint(res, "Failed to synchronize window. \n");
+					break;
+				}
+
+				if((res = viewImageSrc.SynchronizeWindow(ref viewImageDst)).IsFail())
+				{
+					//ErrorPrint(res, "Failed to synchronize window. \n");
+					break;
+				}
+
+				// 이미지 뷰에 이미지를 디스플레이 // Display the image in the image view
+				if((res = viewImageSrc.SetImagePtr(ref fliSrcImage)).IsFail() ||
+					(res = viewImageOpr.SetImagePtr(ref fliOprImage)).IsFail() ||
+					(res = viewImageDst.SetImagePtr(ref fliDstImage)).IsFail())
+				{
+					//ErrorPrint(res, "Failed to set image object on the image view. \n");
+					break;
+				}
+
+
+				// 알고리즘 객체 생성 // Create algorithm object
+				CChannelLpNorm channelLpNorm = new CChannelLpNorm();
+
+				// Source 이미지 설정 // Set Source image
+				if((res = channelLpNorm.SetSourceImage(ref fliSrcImage)).IsFail())
+					break;
+
+				// Operand 이미지 설정 // Set Operand image
+				if((res = channelLpNorm.SetOperandImage(ref fliOprImage)).IsFail())
+					break;
+
+				// Destination 이미지 설정 // Set Destination image 
+				if((res = channelLpNorm.SetDestinationImage(ref fliDstImage)).IsFail())
+					break;
+
+				// 연산 방식 설정 // Set operation source
+				if((res = channelLpNorm.SetOperationSource(EOperationSource.Image)).IsFail())
+					break;
+
+				// 알고리즘 수행 // Execute the algorithm
+				if((res = channelLpNorm.Execute()).IsFail())
+				{
+					//ErrorPrint(res, "Failed to execute the algorithm.");
+					break;
+				}
+
+				// 출력을 위한 이미지 레이어를 얻어옵니다. //  Gets the image layer for output.
+				// 따로 해제할 필요 없음 // No need to release separately
+				CGUIViewImageLayer layerSrc = viewImageSrc.GetLayer(0);
+				CGUIViewImageLayer layerOpr = viewImageOpr.GetLayer(0);
+				CGUIViewImageLayer layerDst = viewImageDst.GetLayer(0);
+
+				// 기존에 Layer에 그려진 도형들을 삭제 // Delete the shapes drawn on the existing layer
+				layerSrc.Clear();
+				layerOpr.Clear();
+				layerDst.Clear();
+
+				// View 정보를 디스플레이 합니다. // Display View information.
+				CFLPoint<double> flpPoint = new CFLPoint<double>(0, 0);
+				if((res = (layerSrc.DrawTextCanvas(flpPoint, "Source Image", EColor.YELLOW, EColor.BLACK, 30))).IsFail() ||
+					(res = layerOpr.DrawTextCanvas(flpPoint, "Operand Image", EColor.YELLOW, EColor.BLACK, 30)).IsFail() ||
+					(res = layerDst.DrawTextCanvas(flpPoint, "Destination Image", EColor.YELLOW, EColor.BLACK, 30)).IsFail())
+				{
+					//ErrorPrint(res, "Failed to draw text. \n");
+					break;
+				}
+
+				// 이미지 뷰를 갱신 합니다. // Update the image view.
+				viewImageSrc.Invalidate(true);
+				viewImageOpr.Invalidate(true);
+				viewImageDst.Invalidate(true);
+
+				// image 가 view 크기에 맞도록 확대 또는 축소합니다. // Zoom image to fit the view.
+				if((res = (viewImageDst.ZoomFit())).IsFail())
+				{
+					//ErrorPrint(res, "Failed to zoom fit. \n");
+					break;
+				}
+
+				// 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
+				while(viewImageSrc.IsAvailable() && viewImageOpr.IsAvailable() && viewImageDst.IsAvailable())
+					;
+			}
+			while(false);
+		}
+	}
+}
