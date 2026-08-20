162af0e161241ba526b50e3d4b33c2b16034f934
shhwang <shhwang@fourthlogic.co.kr>
2026-08-20 19:09:11 +0900

    Add New comparison image in Focus Measurement Example

--- LaplacianVariance/LaplacianVariance.cs
+++ LaplacianVariance/LaplacianVariance.cs
@@ -37,10 +37,12 @@ namespace FLImagingExamplesCSharp
 			// 이미지 객체 선언 // Declare the image object
 			CFLImage fliSrcImage1 = new CFLImage();
 			CFLImage fliSrcImage2 = new CFLImage();
+			CFLImage fliSrcImage3 = new CFLImage();
 
 			// 이미지 뷰 선언 // Declare the image view
 			CGUIViewImage viewImageSrc1 = new CGUIViewImage();
 			CGUIViewImage viewImageSrc2 = new CGUIViewImage();
+			CGUIViewImage viewImageSrc3 = new CGUIViewImage();
 
 			do
 			{
@@ -59,6 +61,11 @@ namespace FLImagingExamplesCSharp
 					break;
 				}
 
+				if((res = fliSrcImage3.Load("../../ExampleImages/FocusMeasurement/Focus3.flif")).IsFail())
+				{
+					ErrorPrint(res, "Failed to load the image file.\n");
+					break;
+				}
 
 				// Source 이미지 뷰 생성 // Create the source image view
 				if((res = viewImageSrc1.Create(400, 0, 912, 612)).IsFail())
@@ -68,6 +75,12 @@ namespace FLImagingExamplesCSharp
 				}
 
 				if((res = viewImageSrc2.Create(912, 0, 1424, 612)).IsFail())
+				{
+					ErrorPrint(res, "Failed to create the image view.\n");
+					break;
+				}
+
+				if((res = viewImageSrc3.Create(1424, 0, 1936, 612)).IsFail())
 				{
 					ErrorPrint(res, "Failed to create the image view.\n");
 					break;
@@ -86,15 +99,33 @@ namespace FLImagingExamplesCSharp
 					break;
 				}
 
-				// 두 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the two image views
+				if((res = viewImageSrc3.SetImagePtr(ref fliSrcImage3)).IsFail())
+				{
+					ErrorPrint(res, "Failed to set image object on the image view.\n");
+					break;
+				}
+
+				// 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the image views
 				if((res = viewImageSrc1.SynchronizePointOfView(ref viewImageSrc2)).IsFail())
 				{
 					ErrorPrint(res, "Failed to synchronize view\n");
 					break;
 				}
 
-				// 두 이미지 뷰 윈도우의 위치를 맞춤 // Synchronize the positions of the two image view windows
+				if((res = viewImageSrc1.SynchronizePointOfView(ref viewImageSrc3)).IsFail())
+				{
+					ErrorPrint(res, "Failed to synchronize view\n");
+					break;
+				}
+
+				// 이미지 뷰 윈도우의 위치를 맞춤 // Synchronize the positions of the image view windows
 				if((res = viewImageSrc1.SynchronizeWindow(ref viewImageSrc2)).IsFail())
+				{
+					ErrorPrint(res, "Failed to synchronize window.\n");
+					break;
+				}
+
+				if((res = viewImageSrc1.SynchronizeWindow(ref viewImageSrc3)).IsFail())
 				{
 					ErrorPrint(res, "Failed to synchronize window.\n");
 					break;
@@ -112,7 +143,7 @@ namespace FLImagingExamplesCSharp
 				// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
 				if((res = laplacianVariance.Execute()).IsFail())
 				{
-                    ErrorPrint(res, "Failed to execute Laplacian Variance.");
+					ErrorPrint(res, "Failed to execute Laplacian Variance.");
 					break;
 				}
 
@@ -132,14 +163,29 @@ namespace FLImagingExamplesCSharp
 				// 결과 점수 획득 // Get Result Score
 				double f64Score2 = laplacianVariance.GetResultScore();
 
+				// Source 이미지 3 설정 // Set the source3 image
+				laplacianVariance.SetSourceImage(ref fliSrcImage3);
+
+				// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+				if((res = laplacianVariance.Execute()).IsFail())
+				{
+					ErrorPrint(res, "Failed to execute Laplacian Variance.");
+					break;
+				}
+
+				// 결과 점수 획득 // Get Result Score
+				double f64Score3 = laplacianVariance.GetResultScore();
+
 				// 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
 				// 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
 				CGUIViewImageLayer layerSrc1 = viewImageSrc1.GetLayer(0);
 				CGUIViewImageLayer layerSrc2 = viewImageSrc2.GetLayer(0);
+				CGUIViewImageLayer layerSrc3 = viewImageSrc3.GetLayer(0);
 
 				// 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
 				layerSrc1.Clear();
 				layerSrc2.Clear();
+				layerSrc3.Clear();
 
 				// 점수를 디스플레이 한다. // Display score
 				// 아래 함수 DrawTextCanvas은 Screen좌표를 기준으로 하는 String을 Drawing 한다. // The function DrawTextCanvas below draws a String based on the screen coordinates.
@@ -148,7 +194,7 @@ namespace FLImagingExamplesCSharp
 				//                 얼라인 -> 폰트 이름 -> 폰트 알파값(불투명도) -> 면 알파값 (불투명도) -> 폰트 두께 -> 폰트 이텔릭
 				// Parameter order: layer -> reference coordinate Figure object -> string -> font color -> Area color -> font size -> actual size -> angle ->
 				//                  Align -> Font Name -> Font Alpha Value (Opaqueness) -> Cotton Alpha Value (Opaqueness) -> Font Thickness -> Font Italic
-				
+
 				if((res = layerSrc1.DrawTextCanvas(new CFLPoint<double>(0, 0), "Score : " + f64Score1, EColor.YELLOW, EColor.BLACK, 20)).IsFail())
 				{
 					ErrorPrint(res, "Failed to draw text.\n");
@@ -161,12 +207,19 @@ namespace FLImagingExamplesCSharp
 					break;
 				}
 
+				if((res = layerSrc3.DrawTextCanvas(new CFLPoint<double>(0, 0), "Score : " + f64Score3, EColor.YELLOW, EColor.BLACK, 20)).IsFail())
+				{
+					ErrorPrint(res, "Failed to draw text.\n");
+					break;
+				}
+
 				// 이미지 뷰를 갱신 합니다. // Update image view
 				viewImageSrc1.Invalidate(true);
 				viewImageSrc2.Invalidate(true);
+				viewImageSrc3.Invalidate(true);
 
 				// 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
-				while(viewImageSrc1.IsAvailable() && viewImageSrc2.IsAvailable())
+				while(viewImageSrc1.IsAvailable() && viewImageSrc2.IsAvailable() && viewImageSrc3.IsAvailable())
 					Thread.Sleep(1);
 			}
 			while(false);
